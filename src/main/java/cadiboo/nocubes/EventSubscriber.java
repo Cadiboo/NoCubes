bf51f2b66e8ee922002daf4c1c10f73b16ff3a02
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-09-23 17:08:42 +1000

    tweaks

--- src/main/java/cadiboo/nocubes/EventSubscriber.java
+++ src/main/java/cadiboo/nocubes/EventSubscriber.java
@@ -1,18 +1,19 @@
 package cadiboo.nocubes;
 
+import com.cosmicdan.nocubes.renderer.SurfaceNets;
+
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber
 public final class EventSubscriber {
 
+	@SubscribeEvent
 	public static void onRenderWorldLastEvent(final RenderWorldLastEvent event) {
 
-		event.setCanceled(true);
-
-		event.setResult(Event.Result.DENY);
-
+		SurfaceNets.renderChunk(0, 1, 1, 1, Minecraft.getMinecraft().world, event.getContext());
 	}
 
 }
