2c98785730a2b3856c964c37132d4d6cd7581967
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-12-02 23:06:17 +1100

    ye nah, no smooth liquids

--- src/main/java/cadiboo/nocubes/EventSubscriber.java
+++ src/main/java/cadiboo/nocubes/EventSubscriber.java
@@ -1,17 +1,31 @@
 package cadiboo.nocubes;
 
 import cadiboo.nocubes.config.ModConfig;
+import cadiboo.nocubes.util.ModUtil;
 import cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockEvent;
 import cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockRenderInLayerEvent;
 import cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockRenderInTypeEvent;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ChunkCache;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class EventSubscriber {
+
+	// rebuildChunkPos -> {liquidBlockPositions}
+	private static final HashMap<BlockPos, HashSet<BlockPos>> renderedLiquidBlocks = new HashMap<BlockPos, HashSet<BlockPos>>();
 
 	@SubscribeEvent(priority = EventPriority.HIGH, receiveCanceled = false)
 	public static void onRebuildChunkBlockEvent(final RebuildChunkBlockEvent event) {
@@ -20,6 +34,68 @@ public class EventSubscriber {
 
 		if (!NoCubes.isEnabled()) {
 			return;
+		}
+
+		if (ModConfig.shouldSmoothLiquids) {
+			final BlockPos blockPos = event.getBlockPos();
+			final BlockPos renderChunkPos = event.getRenderChunkPosition();
+
+			final IBlockState state = event.getBlockState();
+			final Block block = state.getBlock();
+			final ChunkCache chunkCache = event.getChunkCache();
+			final BufferBuilder bufferBuilder = event.getBufferBuilder();
+			if (block instanceof BlockLiquid) {
+
+				for (EnumFacing facing : EnumFacing.VALUES) {
+					final BlockPos facingPos = blockPos.offset(facing);
+					final IBlockState facingState = chunkCache.getBlockState(facingPos);
+					if (!ModUtil.shouldSmooth(facingState)) continue;
+
+					event.getBlockRendererDispatcher().renderBlock(state, facingPos, chunkCache, bufferBuilder);
+
+				}
+
+			}
+
+			// renderChunkPos == currentPos should only be happen once for every chunk
+			// if new chunk (need to clear cache)
+
+//			if (renderedLiquidBlocks.get(blockPos) == null) {
+//				renderedLiquidBlocks.put(renderChunkPos, new HashSet<>());
+//			} else {
+//				// current chunk
+//			}
+//
+//			final HashSet<BlockPos> liquidBlockPositions = renderedLiquidBlocks.get(renderChunkPos);
+//
+//			if (!liquidBlockPositions.contains(blockPos)) {
+//				final IBlockState state = event.getBlockState();
+//				final Block block = state.getBlock();
+//
+//				blockLiquidIf:
+//				if (block instanceof BlockLiquid) {
+//
+//					final ChunkCache chunkCache = event.getChunkCache();
+//
+//					if (!RenderChunkRebuildChunkHooksHooks.canBlockRenderInLayer(event.getRenderChunk(), chunkCache, event.getGenerator(), event.getCompiledChunk(), event.getBlockRendererDispatcher(), event.getRenderChunkPosition(), event.getVisGraph(), event.getBlockPos(), block, state, event.getBlockRenderLayer()))
+//						break blockLiquidIf;
+//
+//					final BufferBuilder bufferBuilder = event.getBufferBuilder();
+//
+//					for (EnumFacing facing : EnumFacing.VALUES) {
+//
+//						final BlockPos facingPos = blockPos.offset(facing);
+//						if (liquidBlockPositions.contains(facingPos)) continue;
+//
+//						final IBlockState facingState = chunkCache.getBlockState(facingPos);
+//						if (!ModUtil.shouldSmooth(facingState)) continue;
+//
+//						liquidBlockPositions.add(facingPos);
+//						event.getBlockRendererDispatcher().renderBlock(state, facingPos, chunkCache, bufferBuilder);
+//
+//					}
+//				}
+//			}
 		}
 
 		event.setCanceled(true);
