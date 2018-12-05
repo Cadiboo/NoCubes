50db5bcd4b084f9297cc724cc38568c411c3d723
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-12-05 12:36:28 +1100

    removed stuff

--- src/main/java/io/github/cadiboo/nocubes/EventSubscriber.java
+++ src/main/java/io/github/cadiboo/nocubes/EventSubscriber.java
@@ -4,7 +4,7 @@ import cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockEvent;
 import cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockRenderInLayerEvent;
 import cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockRenderInTypeEvent;
 import io.github.cadiboo.nocubes.config.ModConfig;
-import net.minecraft.block.BlockAir;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -27,15 +27,27 @@ public class EventSubscriber {
 
 	@SubscribeEvent(priority = EventPriority.HIGH, receiveCanceled = false)
 	public static void onRebuildChunkBlockRenderInLayerEvent(final RebuildChunkBlockRenderInLayerEvent event) {
-		if (event.getBlockState().getBlock() instanceof BlockAir) {
-			event.setResult(Event.Result.ALLOW);
-			event.setCanceled(true);
-		}
+//		if (event.getBlockState().getBlock() instanceof BlockAir) {
+//			event.setResult(Event.Result.ALLOW);
+//			event.setCanceled(true);
+//		}
+
+//		event.setCanceled(true);
+
+//
+//		if (event.getBlockState().getBlock() == Blocks.GRASS) {
+//			if (event.getBlockRenderLayer() == BlockRenderLayer.CUTOUT_MIPPED)
+//				event.setResult(Event.Result.DENY);
+//			else if (event.getBlockRenderLayer() == BlockRenderLayer.TRANSLUCENT)
+//				event.setResult(Event.Result.ALLOW);
+//			event.setCanceled(true);
+//		}
+
 	}
 
 	@SubscribeEvent(priority = EventPriority.HIGH, receiveCanceled = false)
 	public static void onRebuildChunkBlockRenderInTypeEvent(final RebuildChunkBlockRenderInTypeEvent event) {
-		event.setResult(Event.Result.ALLOW);
+//		event.setResult(Event.Result.ALLOW);
 	}
 
 }
