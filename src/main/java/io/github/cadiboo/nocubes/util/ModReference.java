5fb3aeb564337c73049f84b8eb4edc888203c656
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-12-06 22:01:38 +1100

    prep for build

--- src/main/java/io/github/cadiboo/nocubes/util/ModReference.java
+++ src/main/java/io/github/cadiboo/nocubes/util/ModReference.java
@@ -71,7 +71,7 @@ public final class ModReference {
 		 * It is changed when we add new features that are not quite done yet.<br>
 		 * It is reset when we update to a new Minecraft version, our Major Mod Version increments, our Minor Mod Version increments or our Patch Version increments.
 		 */
-		public static final int PRE_RELEASE_VERSION = 12;
+		public static final int PRE_RELEASE_VERSION = 13;
 
 		/**
 		 * This is our Mod's Suffix including our PreRelease Version.<br>
