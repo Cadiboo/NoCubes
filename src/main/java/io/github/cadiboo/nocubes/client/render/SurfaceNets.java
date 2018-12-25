7371e252e288188ca260f2ea0630f679fae840af
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-12-25 11:41:32 +1100

    start on SurfaceNets

--- src/main/java/io/github/cadiboo/nocubes/client/render/SurfaceNets.java
+++ src/main/java/io/github/cadiboo/nocubes/client/render/SurfaceNets.java
@@ -1,5 +1,6 @@
 package io.github.cadiboo.nocubes.client.render;
 
+import io.github.cadiboo.nocubes.client.ClientUtil;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockEvent;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockRenderInLayerEvent;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockRenderInTypeEvent;
@@ -13,6 +14,44 @@ import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPreEvent
  */
 public final class SurfaceNets {
 
+	public static final int[] CUBE_EDGES = new int[24];
+	public static final int[] EDGE_TABLE = new int[256];
+
+	// because the tables are so big we compute them in a static {} instead of hardcoding them
+	//Precompute edge table, like Paul Bourke does.
+	// This saves a bit of time when computing the centroid of each boundary cell
+	static {
+
+		//Initialize the cube_edges table
+		// This is just the vertex number (number of corners) of each cube
+		int k = 0;
+		for (int i = 0; i < 8; ++i) {
+			for (int em = 1; em <= 4; em <<= 1) {
+				int j = i ^ em;
+				if (i <= j) {
+					CUBE_EDGES[k++] = i;
+					CUBE_EDGES[k++] = j;
+				}
+			}
+		}
+
+		// nope, I don't understand this either
+		// yay, Lookup Tables...
+		//Initialize the intersection table.
+		//  This is a 2^(cube configuration) ->  2^(edge configuration) map
+		//  There is one entry for each possible cube configuration, and the output is a 12-bit vector enumerating all edges crossing the 0-level.
+		for (int i = 0; i < 256; ++i) {
+			int em = 0;
+			for (int j = 0; j < 24; j += 2) {
+				final boolean a = (i & (1 << CUBE_EDGES[j])) != 0;
+				final boolean b = (i & (1 << CUBE_EDGES[j + 1])) != 0;
+				em |= a != b ? 1 << (j >> 1) : 0;
+			}
+			EDGE_TABLE[i] = em;
+		}
+
+	}
+
 	public static void renderPre(final RebuildChunkPreEvent event) {
 
 	}
@@ -23,9 +62,13 @@ public final class SurfaceNets {
 
 	public static void renderType(final RebuildChunkBlockRenderInTypeEvent event) {
 
+		ClientUtil.handleTransparentBlocksRenderType(event);
+
 	}
 
 	public static void renderBlock(final RebuildChunkBlockEvent event) {
+
+
 
 	}
 
