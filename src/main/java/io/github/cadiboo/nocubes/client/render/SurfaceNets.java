dab9eda9a3b555c8b5e12bb7436858bc3fd77a36
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-12-25 12:01:44 +1100

    part 3 of Surface nets

--- src/main/java/io/github/cadiboo/nocubes/client/render/SurfaceNets.java
+++ src/main/java/io/github/cadiboo/nocubes/client/render/SurfaceNets.java
@@ -2,6 +2,7 @@ package io.github.cadiboo.nocubes.client.render;
 
 import io.github.cadiboo.nocubes.client.ClientUtil;
 import io.github.cadiboo.nocubes.config.ModConfig;
+import io.github.cadiboo.nocubes.util.LightmapInfo;
 import io.github.cadiboo.nocubes.util.ModUtil;
 import io.github.cadiboo.nocubes.util.Vec3;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockEvent;
@@ -9,6 +10,11 @@ import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockRen
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockRenderInTypeEvent;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPostEvent;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPreEvent;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ChunkCache;
 
@@ -73,8 +79,14 @@ public final class SurfaceNets {
 
 	public static void renderBlock(final RebuildChunkBlockEvent event) {
 
-		final ChunkCache cache = event.getChunkCache();
 		final float isosurfaceLevel = ModConfig.getIsosurfaceLevel();
+		final BlockPos.MutableBlockPos pos = event.getBlockPos();
+		final ChunkCache cache = event.getChunkCache();
+		final IBlockState state = event.getBlockState();
+		final BlockRendererDispatcher blockRendererDispatcher = event.getBlockRendererDispatcher();
+
+		BlockPos.MutableBlockPos texturePos = pos;
+		IBlockState textureState = state;
 
 		final Vec3[] points = new Vec3[]{
 				new Vec3(0.0D, 0.0D, 1.0D),
@@ -106,6 +118,98 @@ public final class SurfaceNets {
 				}
 			}
 			mutablePos.release();
+		}
+
+		// Check for early termination if cell does not intersect boundary
+		// 0x00 = completely inside, 0xFF = completely outside
+		if (neighbourMask == 0b00000000) {
+			event.setCanceled(ModUtil.shouldSmooth(state));
+		} else if (neighbourMask == 0b11111111) {
+			if (ModConfig.hideOutsideBlocks) {
+				event.setCanceled(ModUtil.shouldSmooth(state));
+			}
+		}
+
+		// get texture
+		for (final BlockPos.MutableBlockPos mutablePos : BlockPos.getAllInBoxMutable(pos.add(-1, -1, -1), pos.add(1, 1, 1))) {
+			if (ModUtil.shouldSmooth(textureState)) {
+				break;
+			} else {
+				textureState = cache.getBlockState(mutablePos);
+				texturePos = mutablePos;
+			}
+		}
+
+		final BakedQuad quad = ClientUtil.getQuad(textureState, texturePos, blockRendererDispatcher);
+		if (quad == null) {
+			return;
+		}
+		final TextureAtlasSprite sprite = quad.getSprite();
+		final int color = ClientUtil.getColor(quad, textureState, cache, texturePos);
+		final int red = (color >> 16) & 0xFF;
+		final int green = (color >> 8) & 0xFF;
+		final int blue = color & 0xFF;
+		final int alpha = 0xFF;
+
+		final double minU = ClientUtil.getMinU(sprite);
+		final double minV = ClientUtil.getMinV(sprite);
+		final double maxU = ClientUtil.getMaxU(sprite);
+		final double maxV = ClientUtil.getMaxV(sprite);
+
+		// real pos not texturePos
+		final LightmapInfo lightmapInfo = ClientUtil.getLightmapInfo(pos, cache);
+		final int lightmapSkyLight = lightmapInfo.getLightmapSkyLight();
+		final int lightmapBlockLight = lightmapInfo.getLightmapBlockLight();
+
+		final BufferBuilder bufferBuilder = event.getBufferBuilder();
+
+		// Sum up edge intersections
+		final int edgeMask = EDGE_TABLE[neighbourMask];
+		int edgeCrossingCount = 0;
+		final float[] vertex = new float[]{0.0F, 0.0F, 0.0F};
+
+		// For every edge of the cube...
+		// calculate the interpolated vertex?
+		for (int cubeEdgeIndex = 0; cubeEdgeIndex < 12; ++cubeEdgeIndex) {
+
+			// Use edge mask to check if it is crossed
+			if ((edgeMask & (1 << cubeEdgeIndex)) == 0) {
+				continue;
+			}
+
+			// If it did, increment number of edge crossings
+			++edgeCrossingCount;
+
+			// Now find the point of intersection
+			// The cube edges store the offsets (points) that can contain a smoothable block (I think)
+			final int cubeEdge0 = CUBE_EDGES[cubeEdgeIndex << 1]; // Unpack vertices
+			final int cubeEdge1 = CUBE_EDGES[(cubeEdgeIndex << 1) + 1];
+			final float neighbourDensity0 = neighbourDensities[cubeEdge0]; // Unpack grid values
+			final float neighbourDensity1 = neighbourDensities[cubeEdge1];
+
+			float pointOfIntersection_interpolationValue = neighbourDensity0 - neighbourDensity1; // Compute point of intersection
+			if (Math.abs(pointOfIntersection_interpolationValue) > 0.0F) {
+				pointOfIntersection_interpolationValue = neighbourDensity0 / pointOfIntersection_interpolationValue;
+				int axis = 0;
+
+				// Interpolate vertices and add up intersections (this can be done without multiplying)
+				// because we are dealing with a float[3] instead of a Vec3 (x, y, z),
+				// we use the variable axis to get out index into the float[3]
+				for (int bitIndex = 1; axis < 3; bitIndex <<= 1) {
+					final int a = cubeEdge0 & bitIndex;
+					final int b = cubeEdge1 & bitIndex;
+					// perform linear interpolation efficiently by avoiding multiplication
+					// this can be done because we know that a and b are either 1 or 0, nothing else
+					// this is the same as
+					// vertex[axis] += a * (1.0 - pointOfIntersection_interpolationValue) + b * pointOfIntersection_interpolationValue
+					if (a != b) {
+						vertex[axis] += a != 0 ? 1.0F - pointOfIntersection_interpolationValue : pointOfIntersection_interpolationValue;
+					} else {
+						vertex[axis] += a != 0 ? 1.0F : 0.0F;
+					}
+					++axis;
+				}
+			}
 		}
 
 		
