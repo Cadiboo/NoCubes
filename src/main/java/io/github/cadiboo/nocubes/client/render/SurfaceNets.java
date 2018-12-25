01c1cd2e686bdd1bdd3faebeec17240166fdaf09
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-12-25 12:54:48 +1100

    part 5 of surface nets

--- src/main/java/io/github/cadiboo/nocubes/client/render/SurfaceNets.java
+++ src/main/java/io/github/cadiboo/nocubes/client/render/SurfaceNets.java
@@ -25,40 +25,70 @@ import net.minecraft.world.ChunkCache;
  */
 public final class SurfaceNets {
 
-	public static final int[] CUBE_EDGES = new int[24];
-	public static final int[] EDGE_TABLE = new int[256];
+	private static final int[] CUBE_EDGES = new int[24];
+	private static final int[] EDGE_TABLE = new int[256];
 
 	// because the tables are so big we compute them in a static {} instead of hardcoding them
-	//Precompute edge table, like Paul Bourke does.
-	// This saves a bit of time when computing the centroid of each boundary cell
 	static {
+		generateCubeEdgesTable();
+		generateIntersectionTable();
+	}
+
+	/**
+	 * Utility function to build a table of possible edges for a cube with each
+	 * pair of points representing one edge i.e. [0,1,0,2,0,4,...] would be the
+	 * edges from points 0 to 1, 0 to 2, and 0 to 4 respectively:
+	 *
+	 * <pre>
+	 *  y         z
+	 *  ^        /
+	 *  |
+	 *    6----7
+	 *   /|   /|
+	 *  4----5 |
+	 *  | 2--|-3
+	 *  |/   |/
+	 *  0----1   --> x
+	 * </pre>
+	 */
+	private static void generateCubeEdgesTable() {
 
 		//Initialize the cube_edges table
 		// This is just the vertex number (number of corners) of each cube
-		int k = 0;
-		for (int i = 0; i < 8; ++i) {
+		int cubeEdgesIndex = 0;
+		// 8 is the number of corners for a cube
+		for (int cubeCornerIndex = 0; cubeCornerIndex < 8; ++cubeCornerIndex) {
 			for (int em = 1; em <= 4; em <<= 1) {
-				int j = i ^ em;
-				if (i <= j) {
-					CUBE_EDGES[k++] = i;
-					CUBE_EDGES[k++] = j;
+				int j = cubeCornerIndex ^ em;
+				if (cubeCornerIndex <= j) {
+					SurfaceNets.CUBE_EDGES[cubeEdgesIndex++] = cubeCornerIndex;
+					SurfaceNets.CUBE_EDGES[cubeEdgesIndex++] = j;
 				}
 			}
 		}
+	}
+
+	/**
+	 * Build an intersection table. This is a 2^(cube config) -> 2^(edge config) map
+	 * There is only one entry for each possible cube configuration
+	 * and the output is a 12-bit vector enumerating all edges
+	 * crossing the 0-level
+	 */
+	private static void generateIntersectionTable() {
 
 		// nope, I don't understand this either
 		// yay, Lookup Tables...
-		//Initialize the intersection table.
-		//  This is a 2^(cube configuration) ->  2^(edge configuration) map
-		//  There is one entry for each possible cube configuration, and the output is a 12-bit vector enumerating all edges crossing the 0-level.
-		for (int i = 0; i < 256; ++i) {
+		// Initialize the intersection table.
+		// This is a 2^(cube configuration) ->  2^(edge configuration) map
+		// There is one entry for each possible cube configuration, and the output is a 12-bit vector enumerating all edges crossing the 0-level.
+		for (int edgeTableIndex = 0; edgeTableIndex < 256; ++edgeTableIndex) {
 			int em = 0;
-			for (int j = 0; j < 24; j += 2) {
-				final boolean a = (i & (1 << CUBE_EDGES[j])) != 0;
-				final boolean b = (i & (1 << CUBE_EDGES[j + 1])) != 0;
-				em |= a != b ? 1 << (j >> 1) : 0;
+			for (int cubeEdgesIndex = 0; cubeEdgesIndex < 24; cubeEdgesIndex += 2) {
+				final boolean a = (edgeTableIndex & (1 << CUBE_EDGES[cubeEdgesIndex])) != 0;
+				final boolean b = (edgeTableIndex & (1 << CUBE_EDGES[cubeEdgesIndex + 1])) != 0;
+				em |= a != b ? 1 << (cubeEdgesIndex >> 1) : 0;
 			}
-			EDGE_TABLE[i] = em;
+			EDGE_TABLE[edgeTableIndex] = em;
 		}
 
 	}
@@ -93,15 +123,16 @@ public final class SurfaceNets {
 		BlockPos.MutableBlockPos texturePos = pos;
 		IBlockState textureState = state;
 
+		//TODO: check if this is the right point order
 		final Vec3[] points = new Vec3[]{
-				new Vec3(0.0D, 0.0D, 1.0D),
-				new Vec3(1.0D, 0.0D, 1.0D),
-				new Vec3(1.0D, 0.0D, 0.0D),
-				new Vec3(0.0D, 0.0D, 0.0D),
-				new Vec3(0.0D, 1.0D, 1.0D),
-				new Vec3(1.0D, 1.0D, 1.0D),
-				new Vec3(1.0D, 1.0D, 0.0D),
-				new Vec3(0.0D, 1.0D, 0.0D)
+				new Vec3(0, 0, 1), // 0
+				new Vec3(1, 0, 1), // 1
+				new Vec3(1, 0, 0), // 2
+				new Vec3(0, 0, 0), // 3
+				new Vec3(0, 1, 1), // 4
+				new Vec3(1, 1, 1), // 5
+				new Vec3(1, 1, 0), // 6
+				new Vec3(0, 1, 0)  // 7
 		};
 
 		// Read in 8 field values around this vertex and store them in an array
