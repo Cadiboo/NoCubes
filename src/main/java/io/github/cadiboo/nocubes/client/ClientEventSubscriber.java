6d9d81fecf2c8660c81808d81c88b8a86bc926b6
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-12-28 14:43:13 +1100

    code cleanup

--- src/main/java/io/github/cadiboo/nocubes/client/ClientEventSubscriber.java
+++ src/main/java/io/github/cadiboo/nocubes/client/ClientEventSubscriber.java
@@ -11,13 +11,10 @@ import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPreEvent
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.chunk.CompiledChunk;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-import java.util.Random;
 
 import static net.minecraftforge.fml.relauncher.Side.CLIENT;
 
