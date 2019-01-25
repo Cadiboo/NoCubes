47bde69bb48bcc03c92ffb16aa26c30653890a3d
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-01-25 18:23:08 +1100

    refactor 7

--- src/main/java/io/github/cadiboo/nocubes/util/CacheUtil.java
+++ src/main/java/io/github/cadiboo/nocubes/util/CacheUtil.java
@@ -7,7 +7,12 @@ import javax.annotation.Nonnull;
 
 public final class CacheUtil {
 
-	public static PooledStateCache generateStateCache(final int startPosX, final int startPosY, final int startPosZ, final int cacheSizeX, final int cacheSizeY, final int cacheSizeZ, final IBlockAccess cache, PooledMutableBlockPos pooledMutableBlockPos) {
+	public static PooledStateCache generateStateCache(
+			final int startPosX, final int startPosY, final int startPosZ,
+			final int cacheSizeX, final int cacheSizeY, final int cacheSizeZ,
+			@Nonnull final IBlockAccess cache,
+			@Nonnull PooledMutableBlockPos pooledMutableBlockPos
+	) {
 		final PooledStateCache stateCache = PooledStateCache.retain(cacheSizeX * cacheSizeY * cacheSizeZ);
 		int index = 0;
 		for (int z = 0; z < cacheSizeZ; z++) {
@@ -23,7 +28,9 @@ public final class CacheUtil {
 
 	public static PooledSmoothableCache generateSmoothableCache(
 			final int cacheSizeX, final int cacheSizeY, final int cacheSizeZ,
-			@Nonnull final PooledStateCache stateCache, @Nonnull final IIsSmoothable isStateSmoothable) {
+			@Nonnull final PooledStateCache stateCache,
+			@Nonnull final IIsSmoothable isStateSmoothable
+	) {
 		final PooledSmoothableCache smoothableCache = PooledSmoothableCache.retain(cacheSizeX * cacheSizeY * cacheSizeZ);
 		int index = 0;
 		for (int z = 0; z < cacheSizeZ; z++) {
@@ -37,7 +44,15 @@ public final class CacheUtil {
 		return smoothableCache;
 	}
 
-	private static PooledDensityCache generateDensityCache(final int startPosX, final int startPosY, final int startPosZ, final int densityCacheSizeX, final int densityCacheSizeY, final int densityCacheSizeZ, final PooledStateCache stateCache, final PooledSmoothableCache smoothableCache, final int cacheSizeX, final int cacheSizeY, final int cacheSizeZ, final IBlockAccess cache, final PooledMutableBlockPos pooledMutableBlockPos) {
+	private static PooledDensityCache generateDensityCache(
+			final int startPosX, final int startPosY, final int startPosZ,
+			final int densityCacheSizeX, final int densityCacheSizeY, final int densityCacheSizeZ,
+			@Nonnull final PooledStateCache stateCache,
+			@Nonnull final PooledSmoothableCache smoothableCache,
+			final int cacheSizeX, final int cacheSizeY, final int cacheSizeZ,
+			@Nonnull final IBlockAccess cache,
+			@Nonnull final PooledMutableBlockPos pooledMutableBlockPos
+	) {
 		final PooledDensityCache densityCache = PooledDensityCache.retain(densityCacheSizeX * densityCacheSizeY * densityCacheSizeZ);
 		int index = 0;
 		for (int z = 0; z < densityCacheSizeZ; z++) {
@@ -51,7 +66,15 @@ public final class CacheUtil {
 		return densityCache;
 	}
 
-	private static float getBlockDensity(final int startPosX, final int startPosY, final int startPosZ, final int posX, final int posY, final int posZ, final PooledStateCache stateCache, final PooledSmoothableCache smoothableCache, final int cacheSizeX, final int cacheSizeY, final int cacheSizeZ, final IBlockAccess cache, final PooledMutableBlockPos pooledMutableBlockPos) {
+	private static float getBlockDensity(
+			final int startPosX, final int startPosY, final int startPosZ,
+			final int posX, final int posY, final int posZ,
+			@Nonnull final PooledStateCache stateCache,
+			@Nonnull final PooledSmoothableCache smoothableCache,
+			final int cacheSizeX, final int cacheSizeY, final int cacheSizeZ,
+			@Nonnull final IBlockAccess cache,
+			@Nonnull final PooledMutableBlockPos pooledMutableBlockPos
+	) {
 		float density = 0;
 		// why pre-Increment? We don't know but it works
 		for (int zOffset = 0; zOffset < 2; ++zOffset) {
