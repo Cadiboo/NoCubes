e048aef7b854ffb96c25666390e9eff8e78623b9
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-01-27 12:54:27 +1100

    super slow smooth lighting almost works

--- src/main/java/io/github/cadiboo/nocubes/client/LightmapInfo.java
+++ src/main/java/io/github/cadiboo/nocubes/client/LightmapInfo.java
@@ -4,6 +4,7 @@ import io.github.cadiboo.nocubes.util.Vec3.PooledVec3;
 import net.minecraft.util.math.BlockPos.PooledMutableBlockPos;
 import net.minecraft.world.IBlockAccess;
 
+import static java.lang.Math.floor;
 import static java.lang.Math.round;
 
 /**
@@ -45,20 +46,133 @@ public class LightmapInfo {
 			final IBlockAccess blockAccess, final PooledMutableBlockPos pooledMutableBlockPos
 	) {
 
-		final int packedLight0 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(round(v0.x), round(v0.y), round(v0.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
-		final int packedLight1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(round(v1.x), round(v1.y), round(v1.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
-		final int packedLight2 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(round(v2.x), round(v2.y), round(v2.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
-		final int packedLight3 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(round(v3.x), round(v3.y), round(v3.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight0m10m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v0.x) - 1, floor(v0.y), floor(v0.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight0m100 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v0.x) - 1, floor(v0.y), floor(v0.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight0m11m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v0.x) - 1, floor(v0.y) + 1, floor(v0.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight0m110 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v0.x) - 1, floor(v0.y) + 1, floor(v0.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight000m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v0.x), floor(v0.y), floor(v0.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight0000 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v0.x), floor(v0.y), floor(v0.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight001m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v0.x), floor(v0.y) + 1, floor(v0.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight0010 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v0.x), floor(v0.y) + 1, floor(v0.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+
+		final int packedLight1m10m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v1.x) - 1, floor(v1.y), floor(v1.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight1m100 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v1.x) - 1, floor(v1.y), floor(v1.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight1m11m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v1.x) - 1, floor(v1.y) + 1, floor(v1.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight1m110 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v1.x) - 1, floor(v1.y) + 1, floor(v1.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight100m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v1.x), floor(v1.y), floor(v1.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight1000 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v1.x), floor(v1.y), floor(v1.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight101m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v1.x), floor(v1.y) + 1, floor(v1.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight1010 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v1.x), floor(v1.y) + 1, floor(v1.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+
+		final int packedLight2m10m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v2.x) - 1, floor(v2.y), floor(v2.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight2m100 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v2.x) - 1, floor(v2.y), floor(v2.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight2m11m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v2.x) - 1, floor(v2.y) + 1, floor(v2.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight2m110 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v2.x) - 1, floor(v2.y) + 1, floor(v2.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight200m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v2.x), floor(v2.y), floor(v2.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight2000 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v2.x), floor(v2.y), floor(v2.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight201m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v2.x), floor(v2.y) + 1, floor(v2.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight2010 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v2.x), floor(v2.y) + 1, floor(v2.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+
+		final int packedLight3m10m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v3.x) - 1, floor(v3.y), floor(v3.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight3m100 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v3.x) - 1, floor(v3.y), floor(v3.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight3m11m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v3.x) - 1, floor(v3.y) + 1, floor(v3.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight3m110 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v3.x) - 1, floor(v3.y) + 1, floor(v3.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight300m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v3.x), floor(v3.y), floor(v3.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight3000 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v3.x), floor(v3.y), floor(v3.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight301m1 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v3.x), floor(v3.y) + 1, floor(v3.z) - 1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+		final int packedLight3010 = blockAccess.getBlockState(pooledMutableBlockPos.setPos(floor(v3.x), floor(v3.y) + 1, floor(v3.z))).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+
+		final int skylight0 = max(
+				packedLight0m10m1 >> 16 & 0xFFFF,
+				packedLight0m100 >> 16 & 0xFFFF,
+				packedLight0m11m1 >> 16 & 0xFFFF,
+				packedLight0m110 >> 16 & 0xFFFF,
+				packedLight000m1 >> 16 & 0xFFFF,
+				packedLight0000 >> 16 & 0xFFFF,
+				packedLight001m1 >> 16 & 0xFFFF,
+				packedLight0010 >> 16 & 0xFFFF
+		);
+
+		final int skylight1 = max(
+				packedLight1m10m1 >> 16 & 0xFFFF,
+				packedLight1m100 >> 16 & 0xFFFF,
+				packedLight1m11m1 >> 16 & 0xFFFF,
+				packedLight1m110 >> 16 & 0xFFFF,
+				packedLight100m1 >> 16 & 0xFFFF,
+				packedLight1000 >> 16 & 0xFFFF,
+				packedLight101m1 >> 16 & 0xFFFF,
+				packedLight1010 >> 16 & 0xFFFF
+		);
+
+		final int skylight2 = max(
+				packedLight2m10m1 >> 16 & 0xFFFF,
+				packedLight2m100 >> 16 & 0xFFFF,
+				packedLight2m11m1 >> 16 & 0xFFFF,
+				packedLight2m110 >> 16 & 0xFFFF,
+				packedLight200m1 >> 16 & 0xFFFF,
+				packedLight2000 >> 16 & 0xFFFF,
+				packedLight201m1 >> 16 & 0xFFFF,
+				packedLight2010 >> 16 & 0xFFFF
+		);
+
+		final int skylight3 = max(
+				packedLight3m10m1 >> 16 & 0xFFFF,
+				packedLight3m100 >> 16 & 0xFFFF,
+				packedLight3m11m1 >> 16 & 0xFFFF,
+				packedLight3m110 >> 16 & 0xFFFF,
+				packedLight300m1 >> 16 & 0xFFFF,
+				packedLight3000 >> 16 & 0xFFFF,
+				packedLight301m1 >> 16 & 0xFFFF,
+				packedLight3010 >> 16 & 0xFFFF
+		);
+
+		final int blocklight0 = max(
+				packedLight0m10m1 & 0xFFFF,
+				packedLight0m100 & 0xFFFF,
+				packedLight0m11m1 & 0xFFFF,
+				packedLight0m110 & 0xFFFF,
+				packedLight000m1 & 0xFFFF,
+				packedLight0000 & 0xFFFF,
+				packedLight001m1 & 0xFFFF,
+				packedLight0010 & 0xFFFF
+		);
+
+		final int blocklight1 = max(
+				packedLight1m10m1 & 0xFFFF,
+				packedLight1m100 & 0xFFFF,
+				packedLight1m11m1 & 0xFFFF,
+				packedLight1m110 & 0xFFFF,
+				packedLight100m1 & 0xFFFF,
+				packedLight1000 & 0xFFFF,
+				packedLight101m1 & 0xFFFF,
+				packedLight1010 & 0xFFFF
+		);
+
+		final int blocklight2 = max(
+				packedLight2m10m1 & 0xFFFF,
+				packedLight2m100 & 0xFFFF,
+				packedLight2m11m1 & 0xFFFF,
+				packedLight2m110 & 0xFFFF,
+				packedLight200m1 & 0xFFFF,
+				packedLight2000 & 0xFFFF,
+				packedLight201m1 & 0xFFFF,
+				packedLight2010 & 0xFFFF
+		);
+
+		final int blocklight3 = max(
+				packedLight3m10m1 & 0xFFFF,
+				packedLight3m100 & 0xFFFF,
+				packedLight3m11m1 & 0xFFFF,
+				packedLight3m110 & 0xFFFF,
+				packedLight300m1 & 0xFFFF,
+				packedLight3000 & 0xFFFF,
+				packedLight301m1 & 0xFFFF,
+				packedLight3010 & 0xFFFF
+		);
 
 		return new LightmapInfo(
-				packedLight0 >> 16 & 0xFFFF,
-				packedLight1 >> 16 & 0xFFFF,
-				packedLight2 >> 16 & 0xFFFF,
-				packedLight3 >> 16 & 0xFFFF,
-				packedLight0 & 0xFFFF,
-				packedLight1 & 0xFFFF,
-				packedLight2 & 0xFFFF,
-				packedLight3 & 0xFFFF
+				skylight0, skylight1, skylight2, skylight3,
+				blocklight0, blocklight1, blocklight2, blocklight3
 		);
 
 	}
