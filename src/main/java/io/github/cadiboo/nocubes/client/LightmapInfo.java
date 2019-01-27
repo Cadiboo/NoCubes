a19c5a85d740eb0fd105f238385919bf646e8356
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-01-27 11:45:13 +1100

    I hate lighting

--- src/main/java/io/github/cadiboo/nocubes/client/LightmapInfo.java
+++ src/main/java/io/github/cadiboo/nocubes/client/LightmapInfo.java
@@ -1,11 +1,9 @@
 package io.github.cadiboo.nocubes.client;
 
+import io.github.cadiboo.nocubes.util.Vec3;
 import io.github.cadiboo.nocubes.util.Vec3.PooledVec3;
 import net.minecraft.util.math.BlockPos.PooledMutableBlockPos;
 import net.minecraft.world.IBlockAccess;
-
-import static java.lang.Math.round;
-import static net.minecraft.util.math.MathHelper.clamp;
 
 /**
  * @author Cadiboo
@@ -46,57 +44,142 @@ public class LightmapInfo {
 			final IBlockAccess blockAccess, final PooledMutableBlockPos pooledMutableBlockPos
 	) {
 
-		final int px0 = renderChunkPositionX + pos[0];
-		final int py0 = renderChunkPositionY + pos[1];
-		final int pz0 = renderChunkPositionZ + pos[2];
-		final int px1 = px0 - 1;
-		final int py1 = py0 + 1;
-		final int pz1 = pz0 - 1;
+//		final int px0 = renderChunkPositionX + pos[0];
+//		final int py0 = renderChunkPositionY + pos[1];
+//		final int pz0 = renderChunkPositionZ + pos[2];
+//		final int px1 = px0 + 1;
+//		final int py1 = py0 + 1;
+//		final int pz1 = pz0 + 1;
+//
+//		final int[][][] packedLight = new int[][][]{{
+//				{
+//						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px1, py0, pz1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
+//						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px1, py0, pz0)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
+//				},
+//				{
+//						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px1, py1, pz1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
+//						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px1, py1, pz0)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
+//				}
+//		}, {
+//				{
+//						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px0, py0, pz1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
+//						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px0, py0, pz0)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
+//				},
+//				{
+//						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px0, py1, pz1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
+//						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px0, py1, pz0)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
+//				}
+//		}};
 
-		final int[][][] packedLight = new int[][][]{{
-				{
-						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px1, py0, pz1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
-						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px1, py0, pz0)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
-				},
-				{
-						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px1, py1, pz1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
-						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px1, py1, pz0)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
-				}
-		}, {
-				{
-						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px0, py0, pz1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
-						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px0, py0, pz0)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
-				},
-				{
-						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px0, py1, pz1)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
-						blockAccess.getBlockState(pooledMutableBlockPos.setPos(px0, py1, pz0)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos),
-				}
-		}};
+//		final int v0X = clamp((round(v0.x) - pos[0] - renderChunkPositionX), 0, 1);
+//		final int v0Y = clamp((round(v0.y) - pos[1] - renderChunkPositionY), 0, 1);
+//		final int v0Z = clamp((round(v0.z) - pos[2] - renderChunkPositionZ), 0, 1);
+//		final int v1X = clamp((round(v1.x) - pos[0] - renderChunkPositionX), 0, 1);
+//		final int v1Y = clamp((round(v1.y) - pos[1] - renderChunkPositionY), 0, 1);
+//		final int v1Z = clamp((round(v1.z) - pos[2] - renderChunkPositionZ), 0, 1);
+//		final int v2X = clamp((round(v2.x) - pos[0] - renderChunkPositionX), 0, 1);
+//		final int v2Y = clamp((round(v2.y) - pos[1] - renderChunkPositionY), 0, 1);
+//		final int v2Z = clamp((round(v2.z) - pos[2] - renderChunkPositionZ), 0, 1);
+//		final int v3X = clamp((round(v3.x) - pos[0] - renderChunkPositionX), 0, 1);
+//		final int v3Y = clamp((round(v3.y) - pos[1] - renderChunkPositionY), 0, 1);
+//		final int v3Z = clamp((round(v3.z) - pos[2] - renderChunkPositionZ), 0, 1);
 
-		final int v0X = clamp((round(v0.x) - pos[0] - renderChunkPositionX), 0, 1);
-		final int v0Y = clamp((round(v0.y) - pos[1] - renderChunkPositionY), 0, 1);
-		final int v0Z = clamp((round(v0.z) - pos[2] - renderChunkPositionZ), 0, 1);
-		final int v1X = clamp((round(v1.x) - pos[0] - renderChunkPositionX), 0, 1);
-		final int v1Y = clamp((round(v1.y) - pos[1] - renderChunkPositionY), 0, 1);
-		final int v1Z = clamp((round(v1.z) - pos[2] - renderChunkPositionZ), 0, 1);
-		final int v2X = clamp((round(v2.x) - pos[0] - renderChunkPositionX), 0, 1);
-		final int v2Y = clamp((round(v2.y) - pos[1] - renderChunkPositionY), 0, 1);
-		final int v2Z = clamp((round(v2.z) - pos[2] - renderChunkPositionZ), 0, 1);
-		final int v3X = clamp((round(v3.x) - pos[0] - renderChunkPositionX), 0, 1);
-		final int v3Y = clamp((round(v3.y) - pos[1] - renderChunkPositionY), 0, 1);
-		final int v3Z = clamp((round(v3.z) - pos[2] - renderChunkPositionZ), 0, 1);
+//		final int v0X = floor(v0.x - pos[0] - renderChunkPositionX);
+//		final int v0Y = floor(v0.y - pos[1] - renderChunkPositionY);
+//		final int v0Z = floor(v0.z - pos[2] - renderChunkPositionZ);
+//		final int v1X = floor(v1.x - pos[0] - renderChunkPositionX);
+//		final int v1Y = floor(v1.y - pos[1] - renderChunkPositionY);
+//		final int v1Z = floor(v1.z - pos[2] - renderChunkPositionZ);
+//		final int v2X = floor(v2.x - pos[0] - renderChunkPositionX);
+//		final int v2Y = floor(v2.y - pos[1] - renderChunkPositionY);
+//		final int v2Z = floor(v2.z - pos[2] - renderChunkPositionZ);
+//		final int v3X = floor(v3.x - pos[0] - renderChunkPositionX);
+//		final int v3Y = floor(v3.y - pos[1] - renderChunkPositionY);
+//		final int v3Z = floor(v3.z - pos[2] - renderChunkPositionZ);
+
+//		return new LightmapInfo(
+//				packedLight[v0X][v0Y][v0Z] >> 16 & 0xFFFF,
+//				packedLight[v1X][v1Y][v1Z] >> 16 & 0xFFFF,
+//				packedLight[v2X][v2Y][v2Z] >> 16 & 0xFFFF,
+//				packedLight[v3X][v3Y][v3Z] >> 16 & 0xFFFF,
+//				packedLight[v0X][v0Y][v0Z] & 0xFFFF,
+//				packedLight[v1X][v1Y][v1Z] & 0xFFFF,
+//				packedLight[v2X][v2Y][v2Z] & 0xFFFF,
+//				packedLight[v3X][v3Y][v3Z] & 0xFFFF
+//		);
+
+		final Vec3[] vec3s = {v0, v1, v2, v3};
+
+		final int[] packedLight = new int[36];
+
+		// from (-1, -1, -1) to (1, 1, 1) for each vertex
+		int index = 0;
+		for (int v = 0; v < 4; v++) {
+			for (int z = -1; z < 1; z++) {
+				for (int y = -1; y < 1; y++) {
+					for (int x = -1; x < 1; x++, index++) {
+						final Vec3 vec = vec3s[v];
+						packedLight[index] = blockAccess.getBlockState(pooledMutableBlockPos.setPos(vec.x + x, vec.y + y, vec.z + z)).getPackedLightmapCoords(blockAccess, pooledMutableBlockPos);
+					}
+				}
+			}
+		}
+
+		final int[] skyLight = new int[4];
+		final int[] blockLight = new int[4];
+		for (int v = 0; v < 4; ++v) {
+			index = v * 4;
+			if ((packedLight[index] >> 16 & 0xFFFF) != 0) {
+				skyLight[v] = packedLight[index] >> 16 & 0xFFFF;
+			} else {
+				skyLight[v] = max(
+						packedLight[index] >> 16 & 0xFFFF,
+						packedLight[index + 1] >> 16 & 0xFFFF,
+						packedLight[index + 2] >> 16 & 0xFFFF,
+						packedLight[index + 3] >> 16 & 0xFFFF,
+						packedLight[index + 4] >> 16 & 0xFFFF,
+						packedLight[index + 5] >> 16 & 0xFFFF,
+						packedLight[index + 6] >> 16 & 0xFFFF,
+						packedLight[index + 7] >> 16 & 0xFFFF,
+						packedLight[index + 8] >> 16 & 0xFFFF
+				);
+			}
+			if ((packedLight[index] & 0xFFFF) != 0) {
+				blockLight[v] = packedLight[index] & 0xFFFF;
+			} else {
+				blockLight[v] = max(
+						packedLight[index] & 0xFFFF,
+						packedLight[index + 1] & 0xFFFF,
+						packedLight[index + 2] & 0xFFFF,
+						packedLight[index + 3] & 0xFFFF,
+						packedLight[index + 4] & 0xFFFF,
+						packedLight[index + 5] & 0xFFFF,
+						packedLight[index + 6] & 0xFFFF,
+						packedLight[index + 7] & 0xFFFF,
+						packedLight[index + 8] & 0xFFFF
+				);
+			}
+		}
 
 		return new LightmapInfo(
-				packedLight[v0X][v0Y][v0Z] >> 16 & 0xFFFF,
-				packedLight[v1X][v1Y][v1Z] >> 16 & 0xFFFF,
-				packedLight[v2X][v2Y][v2Z] >> 16 & 0xFFFF,
-				packedLight[v3X][v3Y][v3Z] >> 16 & 0xFFFF,
-				packedLight[v0X][v0Y][v0Z] & 0xFFFF,
-				packedLight[v1X][v1Y][v1Z] & 0xFFFF,
-				packedLight[v2X][v2Y][v2Z] & 0xFFFF,
-				packedLight[v3X][v3Y][v3Z] & 0xFFFF
+				skyLight[0],
+				skyLight[1],
+				skyLight[2],
+				skyLight[3],
+				blockLight[0],
+				blockLight[1],
+				blockLight[2],
+				blockLight[3]
 		);
 
+	}
+
+	public static final int max(int... ints) {
+		int max = 0;
+		for (final int anInt : ints) {
+			if (max < anInt) max = anInt;
+		}
+		return max;
 	}
 
 	//FIXME: MAX PRIORITY FOR 0.2.0
