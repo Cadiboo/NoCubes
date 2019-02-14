fd2e2fac9e4c2e63aa436c5d25ee12b719061a5e
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-02-14 22:56:03 +1100

    Update ModProfiler.java

--- src/main/java/io/github/cadiboo/nocubes/util/ModProfiler.java
+++ src/main/java/io/github/cadiboo/nocubes/util/ModProfiler.java
@@ -1,11 +1,11 @@
-package io.github.cadiboo.nocubes.util;
-
-import net.minecraft.profiler.Profiler;
-
-public class ModProfiler extends Profiler {
-
-	public void putSection(final String sectionName, final long estimatedTimeTakenNanoseconds) {
-//		NoCubes.NO_CUBES_LOG.debug(sectionName + " took approximately " + estimatedTimeTakenNanoseconds + " nanoseconds");
-	}
-
-}
+//package io.github.cadiboo.nocubes.util;
+//
+//import net.minecraft.profiler.Profiler;
+//
+//public class ModProfiler extends Profiler {
+//
+//	public void putSection(final String sectionName, final long estimatedTimeTakenNanoseconds) {
+////		NoCubes.NO_CUBES_LOG.debug(sectionName + " took approximately " + estimatedTimeTakenNanoseconds + " nanoseconds");
+//	}
+//
+//}
