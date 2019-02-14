4377ffd9ec6feef98ef6d4752acf9c7c923b0ff9
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-02-14 23:28:36 +1100

    Update PooledStateCache.java

--- src/main/java/io/github/cadiboo/nocubes/unusedpools/PooledStateCache.java
+++ src/main/java/io/github/cadiboo/nocubes/unusedpools/PooledStateCache.java
@@ -1,65 +1,65 @@
-package io.github.cadiboo.nocubes.unusedpools;
-
-import net.minecraft.block.state.IBlockState;
-import org.apache.logging.log4j.LogManager;
-
-import java.util.ArrayList;
-
-/**
- * @author Cadiboo
- */
-public class PooledStateCache implements AutoCloseable {
-
-	private IBlockState[] stateCache;
-	private boolean released;
-	private static final ArrayList<PooledStateCache> POOL = new ArrayList<>();
-
-	private PooledStateCache(int size) {
-		stateCache = new IBlockState[size];
-	}
-
-	public static PooledStateCache retain(final int size) {
-		synchronized (POOL) {
-			if (!POOL.isEmpty()) {
-				PooledStateCache pooledStateCache = POOL.remove(POOL.size() - 1);
-
-				if (pooledStateCache != null && pooledStateCache.released) {
-					pooledStateCache.released = false;
-					pooledStateCache.setSize(size);
-					return pooledStateCache;
-				}
-			}
-		}
-
-		return new PooledStateCache(size);
-	}
-
-	private void setSize(final int size) {
-		if (stateCache.length != size) {
-			stateCache = new IBlockState[size];
-		}
-	}
-
-	public void release() {
-		synchronized (POOL) {
-			if (POOL.size() < 100) {
-				POOL.add(this);
-			}
-
-			this.released = true;
-		}
-	}
-
-	public IBlockState[] getStateCache() {
-		if (this.released) {
-			LogManager.getLogger().error("PooledStateCache gotten after it was released!", new Throwable());
-		}
-		return stateCache;
-	}
-
-	@Override
-	public void close() {
-		this.release();
-	}
-
-}
+//package io.github.cadiboo.nocubes.unusedpools;
+//
+//import net.minecraft.block.state.IBlockState;
+//import org.apache.logging.log4j.LogManager;
+//
+//import java.util.ArrayList;
+//
+///**
+// * @author Cadiboo
+// */
+//public class PooledStateCache implements AutoCloseable {
+//
+//	private IBlockState[] stateCache;
+//	private boolean released;
+//	private static final ArrayList<PooledStateCache> POOL = new ArrayList<>();
+//
+//	private PooledStateCache(int size) {
+//		stateCache = new IBlockState[size];
+//	}
+//
+//	public static PooledStateCache retain(final int size) {
+//		synchronized (POOL) {
+//			if (!POOL.isEmpty()) {
+//				PooledStateCache pooledStateCache = POOL.remove(POOL.size() - 1);
+//
+//				if (pooledStateCache != null && pooledStateCache.released) {
+//					pooledStateCache.released = false;
+//					pooledStateCache.setSize(size);
+//					return pooledStateCache;
+//				}
+//			}
+//		}
+//
+//		return new PooledStateCache(size);
+//	}
+//
+//	private void setSize(final int size) {
+//		if (stateCache.length != size) {
+//			stateCache = new IBlockState[size];
+//		}
+//	}
+//
+//	public void release() {
+//		synchronized (POOL) {
+//			if (POOL.size() < 100) {
+//				POOL.add(this);
+//			}
+//
+//			this.released = true;
+//		}
+//	}
+//
+//	public IBlockState[] getStateCache() {
+//		if (this.released) {
+//			LogManager.getLogger().error("PooledStateCache gotten after it was released!", new Throwable());
+//		}
+//		return stateCache;
+//	}
+//
+//	@Override
+//	public void close() {
+//		this.release();
+//	}
+//
+//}
