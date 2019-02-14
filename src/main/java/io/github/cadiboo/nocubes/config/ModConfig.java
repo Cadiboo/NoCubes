e71d7749b4f86b4b9ce8fe9d734fe72b1414da43
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-02-14 22:49:21 +1100

    config

--- src/main/java/io/github/cadiboo/nocubes/config/ModConfig.java
+++ src/main/java/io/github/cadiboo/nocubes/config/ModConfig.java
@@ -4,73 +4,51 @@ import io.github.cadiboo.nocubes.NoCubes;
 import io.github.cadiboo.nocubes.client.ExtendLiquidRange;
 import io.github.cadiboo.nocubes.mesh.MeshGenerator;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockDirt;
-import net.minecraft.block.BlockGrass;
-import net.minecraft.block.BlockMycelium;
-import net.minecraft.block.BlockRedSandstone;
-import net.minecraft.block.BlockSand;
-import net.minecraft.block.BlockSandStone;
-import net.minecraft.block.BlockSilverfish;
-import net.minecraft.block.BlockSnow;
-import net.minecraft.block.BlockStainedHardenedClay;
-import net.minecraft.block.BlockStone;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.Minecraft;
-import net.minecraft.command.CommandBase;
-import net.minecraft.command.InvalidBlockStateException;
-import net.minecraft.command.NumberInvalidException;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.Config.LangKey;
-import net.minecraftforge.common.config.ConfigManager;
-import net.minecraftforge.fml.client.event.ConfigChangedEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
-import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.GrassBlock;
+import net.minecraft.block.MyceliumBlock;
+import net.minecraft.block.SnowBlock;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 
 import static io.github.cadiboo.nocubes.util.ModReference.MOD_ID;
-import static net.minecraft.init.Blocks.BEDROCK;
-import static net.minecraft.init.Blocks.CLAY;
-import static net.minecraft.init.Blocks.COAL_ORE;
-import static net.minecraft.init.Blocks.DIAMOND_ORE;
-import static net.minecraft.init.Blocks.DIRT;
-import static net.minecraft.init.Blocks.EMERALD_ORE;
-import static net.minecraft.init.Blocks.END_STONE;
-import static net.minecraft.init.Blocks.GLOWSTONE;
-import static net.minecraft.init.Blocks.GOLD_ORE;
-import static net.minecraft.init.Blocks.GRASS;
-import static net.minecraft.init.Blocks.GRASS_PATH;
-import static net.minecraft.init.Blocks.GRAVEL;
-import static net.minecraft.init.Blocks.HARDENED_CLAY;
-import static net.minecraft.init.Blocks.IRON_ORE;
-import static net.minecraft.init.Blocks.LAPIS_ORE;
-import static net.minecraft.init.Blocks.LIT_REDSTONE_ORE;
-import static net.minecraft.init.Blocks.MAGMA;
-import static net.minecraft.init.Blocks.MONSTER_EGG;
-import static net.minecraft.init.Blocks.MYCELIUM;
-import static net.minecraft.init.Blocks.NETHERRACK;
-import static net.minecraft.init.Blocks.QUARTZ_ORE;
-import static net.minecraft.init.Blocks.REDSTONE_ORE;
-import static net.minecraft.init.Blocks.RED_SANDSTONE;
-import static net.minecraft.init.Blocks.SAND;
-import static net.minecraft.init.Blocks.SANDSTONE;
-import static net.minecraft.init.Blocks.SNOW;
-import static net.minecraft.init.Blocks.SNOW_LAYER;
-import static net.minecraft.init.Blocks.SOUL_SAND;
-import static net.minecraft.init.Blocks.STAINED_HARDENED_CLAY;
-import static net.minecraft.init.Blocks.STONE;
-import static net.minecraft.item.EnumDyeColor.BLACK;
-import static net.minecraft.item.EnumDyeColor.BROWN;
-import static net.minecraft.item.EnumDyeColor.ORANGE;
-import static net.minecraft.item.EnumDyeColor.RED;
-import static net.minecraft.item.EnumDyeColor.SILVER;
-import static net.minecraft.item.EnumDyeColor.WHITE;
-import static net.minecraft.item.EnumDyeColor.YELLOW;
+import static net.minecraft.block.Blocks.BEDROCK;
+import static net.minecraft.block.Blocks.CLAY;
+import static net.minecraft.block.Blocks.COAL_ORE;
+import static net.minecraft.block.Blocks.COARSE_DIRT;
+import static net.minecraft.block.Blocks.DIAMOND_ORE;
+import static net.minecraft.block.Blocks.DIRT;
+import static net.minecraft.block.Blocks.EMERALD_ORE;
+import static net.minecraft.block.Blocks.END_STONE;
+import static net.minecraft.block.Blocks.GLOWSTONE;
+import static net.minecraft.block.Blocks.GOLD_ORE;
+import static net.minecraft.block.Blocks.GRASS;
+import static net.minecraft.block.Blocks.GRASS_PATH;
+import static net.minecraft.block.Blocks.GRAVEL;
+import static net.minecraft.block.Blocks.INFESTED_CHISELED_STONE_BRICKS;
+import static net.minecraft.block.Blocks.INFESTED_COBBLESTONE;
+import static net.minecraft.block.Blocks.INFESTED_CRACKED_STONE_BRICKS;
+import static net.minecraft.block.Blocks.*;
+import static net.minecraft.block.Blocks.INFESTED_STONE;
+import static net.minecraft.block.Blocks.INFESTED_STONE_BRICKS;
+import static net.minecraft.block.Blocks.IRON_ORE;
+import static net.minecraft.block.Blocks.LAPIS_ORE;
+import static net.minecraft.block.Blocks.MYCELIUM;
+import static net.minecraft.block.Blocks.NETHERRACK;
+import static net.minecraft.block.Blocks.NETHER_QUARTZ_ORE;
+import static net.minecraft.block.Blocks.PODZOL;
+import static net.minecraft.block.Blocks.REDSTONE_ORE;
+import static net.minecraft.block.Blocks.RED_SANDSTONE;
+import static net.minecraft.block.Blocks.SAND;
+import static net.minecraft.block.Blocks.SANDSTONE;
+import static net.minecraft.block.Blocks.SNOW;
+import static net.minecraft.block.Blocks.SOUL_SAND;
+import static net.minecraft.block.Blocks.STONE;
+import static net.minecraft.block.Blocks.TERRACOTTA;
+import static net.minecraft.util.DyeColor.ORANGE;
+import static net.minecraft.util.DyeColor.*;
 
 /**
  * Our Mod's configuration
@@ -78,61 +56,61 @@ import static net.minecraft.item.EnumDyeColor.YELLOW;
  * @author Cadiboo
  */
 @SuppressWarnings("WeakerAccess")
-@Config(modid = MOD_ID)
-@LangKey(MOD_ID + ".config.title")
+//@Config(modid = MOD_ID)
+//@LangKey(MOD_ID + ".config.title")
 public final class ModConfig {
 
-	@Config.Ignore
-	private static final transient HashSet<IBlockState> SMOOTHABLE_BLOCK_STATES_CACHE = new HashSet<>();
+	//@Config.Ignore
+	private static final transient HashSet<BlockState> SMOOTHABLE_BLOCK_STATES_CACHE = new HashSet<>();
 
-	@LangKey(MOD_ID + ".config.isEnabled")
+	//@LangKey(MOD_ID + ".config.isEnabled")
 	public static boolean isEnabled = true;
 
-	@LangKey(MOD_ID + ".config.meshGenerator")
+	//@LangKey(MOD_ID + ".config.meshGenerator")
 	public static MeshGenerator meshGenerator = MeshGenerator.SurfaceNets;
 
-	@LangKey(MOD_ID + ".config.reloadChunksOnConfigChange")
+	//@LangKey(MOD_ID + ".config.reloadChunksOnConfigChange")
 	public static boolean reloadChunksOnConfigChange = true;
 
-	@LangKey(MOD_ID + ".config.smoothableBlockStates")
+	//@LangKey(MOD_ID + ".config.smoothableBlockStates")
 	public static String[] smoothableBlockStates;
 
-	//	@LangKey(MOD_ID + ".config.isosurfaceLevel")
-//	@Config.RangeDouble(min = -10, max = 10)
-	@Config.Ignore
+	//	//@LangKey(MOD_ID + ".config.isosurfaceLevel")
+//	//@Config.RangeDouble(min = -10, max = 10)
+	//@Config.Ignore
 	public static double isosurfaceLevel = 1.0D;
 
-	//	@LangKey(MOD_ID + ".config.offsetVertices")
-	@Config.Ignore
+	//	//@LangKey(MOD_ID + ".config.offsetVertices")
+	//@Config.Ignore
 	public static boolean offsetVertices = false;
 
-	//	@LangKey(MOD_ID + ".config.offsetAmmount")
-//	@Config.RangeDouble(min = -10, max = 10)
-	@Config.Ignore
+	//	//@LangKey(MOD_ID + ".config.offsetAmmount")
+//	//@Config.RangeDouble(min = -10, max = 10)
+	//@Config.Ignore
 	public static double offsetAmmount = 0.5F;
 
-	@LangKey(MOD_ID + ".config.approximateLighting")
+	//@LangKey(MOD_ID + ".config.approximateLighting")
 	public static boolean approximateLighting = true;
 
-	@LangKey(MOD_ID + ".config.smoothLeavesSeparate")
+	//@LangKey(MOD_ID + ".config.smoothLeavesSeparate")
 	public static boolean smoothLeavesSeparate = true;
 
-	@LangKey(MOD_ID + ".config.extendLiquids")
+	//@LangKey(MOD_ID + ".config.extendLiquids")
 	public static ExtendLiquidRange extendLiquids = ExtendLiquidRange.OneBlock;
 
-	@Config.Ignore
+	//@Config.Ignore
 	//yenah i dont like it //TODO: remove
 	public static boolean renderEmptyBlocksOrWhatever = false;
 
-	@Config.Ignore
-//	@Config.RangeDouble(min = -10, max = 10)
+	//@Config.Ignore
+//	//@Config.RangeDouble(min = -10, max = 10)
 	public static double smoothOtherBlocksAmount = 0.0F;
 
 	static {
 		setupSmoothableBlockStates();
 	}
 
-	public static HashSet<IBlockState> getSmoothableBlockStatesCache() {
+	public static HashSet<BlockState> getSmoothableBlockStatesCache() {
 		return SMOOTHABLE_BLOCK_STATES_CACHE;
 	}
 
@@ -151,83 +129,83 @@ public final class ModConfig {
 		return meshGenerator;
 	}
 
-	@Mod.EventBusSubscriber(modid = MOD_ID)
-	private static class ConfigEventSubscriber {
-
-		/**
-		 * Inject the new values and save to the config file when the config has been changed from the GUI.
-		 *
-		 * @param event The event
-		 */
-		@SubscribeEvent
-		public static void onConfigChanged(final ConfigChangedEvent.OnConfigChangedEvent event) {
-
-			if (event.getModID().equals(MOD_ID)) {
-				ConfigManager.sync(MOD_ID, Config.Type.INSTANCE);
-
-				if (reloadChunksOnConfigChange) {
-					if (Minecraft.getMinecraft().renderGlobal != null) {
-						Minecraft.getMinecraft().renderGlobal.loadRenderers();
-					}
-				}
-
-				SMOOTHABLE_BLOCK_STATES_CACHE.clear();
-
-				final IForgeRegistry<Block> BLOCKS = ForgeRegistries.BLOCKS;
-
-				for (String blockStateString : smoothableBlockStates) {
-					final String[] splitBlockStateString = StringUtils.split(blockStateString, "[");
-					final String blockString = splitBlockStateString[0];
-					final String stateString;
-					if (splitBlockStateString.length == 1) {
-						stateString = "default";
-					} else if (splitBlockStateString.length == 2) {
-						stateString = StringUtils.reverse(StringUtils.reverse(StringUtils.split(blockStateString, "[")[1]).replaceFirst("]", ""));
-					} else {
-						NoCubes.NO_CUBES_LOG.error("Block/Blockstate Parsing error for \"" + blockStateString + "\"");
-						continue;
-					}
-
-					final Block block = BLOCKS.getValue(new ResourceLocation(blockString));
-					if (block == null) {
-						NoCubes.NO_CUBES_LOG.error("Block Parsing error NullPointerException for \"" + blockString + "\"");
-						continue;
-					}
-					try {
-						SMOOTHABLE_BLOCK_STATES_CACHE.add(CommandBase.convertArgToBlockState(block, stateString));
-					} catch (NumberInvalidException | InvalidBlockStateException e) {
-						NoCubes.NO_CUBES_LOG.error("Blockstate Parsing error " + e + " for \"" + stateString + "\"");
-						continue;
-					}
-
-				}
-
-			}
-
-		}
-
-	}
+//	@Mod.EventBusSubscriber(modid = MOD_ID)
+//	private static class ConfigEventSubscriber {
+//
+//		/**
+//		 * Inject the new values and save to the config file when the config has been changed from the GUI.
+//		 *
+//		 * @param event The event
+//		 */
+//		@SubscribeEvent
+//		public static void onConfigChanged(final ConfigChangedEvent.OnConfigChangedEvent event) {
+//
+//			if (event.getModID().equals(MOD_ID)) {
+//				ConfigManager.sync(MOD_ID, Config.Type.INSTANCE);
+//
+//				if (reloadChunksOnConfigChange) {
+//					if (Minecraft.getMinecraft().renderGlobal != null) {
+//						Minecraft.getMinecraft().renderGlobal.loadRenderers();
+//					}
+//				}
+//
+//				SMOOTHABLE_BLOCK_STATES_CACHE.clear();
+//
+//				final IForgeRegistry<Block> BLOCKS = ForgeRegistries.BLOCKS;
+//
+//				for (String blockStateString : smoothableBlockStates) {
+//					final String[] splitBlockStateString = StringUtils.split(blockStateString, "[");
+//					final String blockString = splitBlockStateString[0];
+//					final String stateString;
+//					if (splitBlockStateString.length == 1) {
+//						stateString = "default";
+//					} else if (splitBlockStateString.length == 2) {
+//						stateString = StringUtils.reverse(StringUtils.reverse(StringUtils.split(blockStateString, "[")[1]).replaceFirst("]", ""));
+//					} else {
+//						NoCubes.NO_CUBES_LOG.error("Block/Blockstate Parsing error for \"" + blockStateString + "\"");
+//						continue;
+//					}
+//
+//					final Block block = BLOCKS.getValue(new ResourceLocation(blockString));
+//					if (block == null) {
+//						NoCubes.NO_CUBES_LOG.error("Block Parsing error NullPointerException for \"" + blockString + "\"");
+//						continue;
+//					}
+//					try {
+//						SMOOTHABLE_BLOCK_STATES_CACHE.add(CommandBase.convertArgToBlockState(block, stateString));
+//					} catch (NumberInvalidException | InvalidBlockStateException e) {
+//						NoCubes.NO_CUBES_LOG.error("Blockstate Parsing error " + e + " for \"" + stateString + "\"");
+//						continue;
+//					}
+//
+//				}
+//
+//			}
+//
+//		}
+//
+//	}
 
 	private static void setupSmoothableBlockStates() {
-		final IBlockState[] defaultSmoothableBlockStates = new IBlockState[]{
+		final BlockState[] defaultSmoothableBlockStates = new BlockState[]{
 
-				GRASS.getDefaultState().withProperty(BlockGrass.SNOWY, true),
-				GRASS.getDefaultState().withProperty(BlockGrass.SNOWY, false),
+				GRASS.getDefaultState().with(GrassBlock.SNOWY, true),
+				GRASS.getDefaultState().with(GrassBlock.SNOWY, false),
 
-				STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.STONE),
-				STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.GRANITE),
-				STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.GRANITE_SMOOTH),
-				STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.DIORITE),
-				STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.DIORITE_SMOOTH),
-				STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.ANDESITE),
-				STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.ANDESITE_SMOOTH),
+				STONE.getDefaultState(),
+				GRANITE.getDefaultState(),
+				POLISHED_GRANITE.getDefaultState(),
+				DIORITE.getDefaultState(),
+				POLISHED_DIORITE.getDefaultState(),
+				ANDESITE.getDefaultState(),
+				POLISHED_ANDESITE.getDefaultState(),
 
-				SAND.getDefaultState().withProperty(BlockSand.VARIANT, BlockSand.EnumType.SAND),
-				SAND.getDefaultState().withProperty(BlockSand.VARIANT, BlockSand.EnumType.RED_SAND),
+				SAND.getDefaultState(),
+				RED_SAND.getDefaultState(),
 
-				SANDSTONE.getDefaultState().withProperty(BlockSandStone.TYPE, BlockSandStone.EnumType.DEFAULT),
+				SANDSTONE.getDefaultState(),
 
-				RED_SANDSTONE.getDefaultState().withProperty(BlockRedSandstone.TYPE, BlockRedSandstone.EnumType.DEFAULT),
+				RED_SANDSTONE.getDefaultState(),
 
 				GRAVEL.getDefaultState(),
 
@@ -235,64 +213,63 @@ public final class ModConfig {
 				IRON_ORE.getDefaultState(),
 				GOLD_ORE.getDefaultState(),
 				REDSTONE_ORE.getDefaultState(),
-				LIT_REDSTONE_ORE.getDefaultState(),
 				DIAMOND_ORE.getDefaultState(),
 				LAPIS_ORE.getDefaultState(),
 				EMERALD_ORE.getDefaultState(),
-				QUARTZ_ORE.getDefaultState(),
+				NETHER_QUARTZ_ORE.getDefaultState(),
 
-				MONSTER_EGG.getDefaultState().withProperty(BlockSilverfish.VARIANT, BlockSilverfish.EnumType.STONE),
-				MONSTER_EGG.getDefaultState().withProperty(BlockSilverfish.VARIANT, BlockSilverfish.EnumType.COBBLESTONE),
-				MONSTER_EGG.getDefaultState().withProperty(BlockSilverfish.VARIANT, BlockSilverfish.EnumType.STONEBRICK),
-				MONSTER_EGG.getDefaultState().withProperty(BlockSilverfish.VARIANT, BlockSilverfish.EnumType.MOSSY_STONEBRICK),
-				MONSTER_EGG.getDefaultState().withProperty(BlockSilverfish.VARIANT, BlockSilverfish.EnumType.CRACKED_STONEBRICK),
-				MONSTER_EGG.getDefaultState().withProperty(BlockSilverfish.VARIANT, BlockSilverfish.EnumType.CHISELED_STONEBRICK),
+				INFESTED_STONE.getDefaultState(),
+				INFESTED_COBBLESTONE.getDefaultState(),
+				INFESTED_STONE_BRICKS.getDefaultState(),
+				INFESTED_MOSSY_STONE_BRICKS.getDefaultState(),
+				INFESTED_CRACKED_STONE_BRICKS.getDefaultState(),
+				INFESTED_CHISELED_STONE_BRICKS.getDefaultState(),
 
 				GRASS_PATH.getDefaultState(),
 
-				DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.DIRT),
-				DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.COARSE_DIRT),
-				DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.PODZOL),
+				DIRT.getDefaultState(),
+				COARSE_DIRT.getDefaultState(),
+				PODZOL.getDefaultState(),
 
 				CLAY.getDefaultState(),
-				HARDENED_CLAY.getDefaultState(),
+				TERRACOTTA.getDefaultState(),
 
-				STAINED_HARDENED_CLAY.getDefaultState().withProperty(BlockStainedHardenedClay.COLOR, WHITE),
-				STAINED_HARDENED_CLAY.getDefaultState().withProperty(BlockStainedHardenedClay.COLOR, ORANGE),
-				STAINED_HARDENED_CLAY.getDefaultState().withProperty(BlockStainedHardenedClay.COLOR, YELLOW),
-				STAINED_HARDENED_CLAY.getDefaultState().withProperty(BlockStainedHardenedClay.COLOR, SILVER),
-				STAINED_HARDENED_CLAY.getDefaultState().withProperty(BlockStainedHardenedClay.COLOR, BROWN),
-				STAINED_HARDENED_CLAY.getDefaultState().withProperty(BlockStainedHardenedClay.COLOR, RED),
-				STAINED_HARDENED_CLAY.getDefaultState().withProperty(BlockStainedHardenedClay.COLOR, BLACK),
+				WHITE_TERRACOTTA.getDefaultState(),
+				ORANGE_TERRACOTTA.getDefaultState(),
+				YELLOW_TERRACOTTA.getDefaultState(),
+				GRAY_TERRACOTTA.getDefaultState(),
+				BROWN_TERRACOTTA.getDefaultState(),
+				RED_TERRACOTTA.getDefaultState(),
+				BLACK_TERRACOTTA.getDefaultState(),
 
 				SNOW.getDefaultState(),
 
-				SNOW_LAYER.getDefaultState().withProperty(BlockSnow.LAYERS, 1),
-				SNOW_LAYER.getDefaultState().withProperty(BlockSnow.LAYERS, 2),
-				SNOW_LAYER.getDefaultState().withProperty(BlockSnow.LAYERS, 3),
-				SNOW_LAYER.getDefaultState().withProperty(BlockSnow.LAYERS, 4),
-				SNOW_LAYER.getDefaultState().withProperty(BlockSnow.LAYERS, 5),
-				SNOW_LAYER.getDefaultState().withProperty(BlockSnow.LAYERS, 6),
-				SNOW_LAYER.getDefaultState().withProperty(BlockSnow.LAYERS, 7),
-				SNOW_LAYER.getDefaultState().withProperty(BlockSnow.LAYERS, 8),
+				SNOW.getDefaultState().with(SnowBlock.LAYERS, 1),
+				SNOW.getDefaultState().with(SnowBlock.LAYERS, 2),
+				SNOW.getDefaultState().with(SnowBlock.LAYERS, 3),
+				SNOW.getDefaultState().with(SnowBlock.LAYERS, 4),
+				SNOW.getDefaultState().with(SnowBlock.LAYERS, 5),
+				SNOW.getDefaultState().with(SnowBlock.LAYERS, 6),
+				SNOW.getDefaultState().with(SnowBlock.LAYERS, 7),
+				SNOW.getDefaultState().with(SnowBlock.LAYERS, 8),
 
 				BEDROCK.getDefaultState(),
 
 				NETHERRACK.getDefaultState(),
 				SOUL_SAND.getDefaultState(),
-				MAGMA.getDefaultState(),
+				MAGMA_BLOCK.getDefaultState(),
 				GLOWSTONE.getDefaultState(),
 
 				END_STONE.getDefaultState(),
 
-				MYCELIUM.getDefaultState().withProperty(BlockMycelium.SNOWY, true),
-				MYCELIUM.getDefaultState().withProperty(BlockMycelium.SNOWY, false),
+				MYCELIUM.getDefaultState().with(MyceliumBlock.SNOWY, true),
+				MYCELIUM.getDefaultState().with(MyceliumBlock.SNOWY, false),
 
 		};
 
 		final ArrayList<String> tempSmoothableBlockStates = new ArrayList<>();
 
-		for (IBlockState state : defaultSmoothableBlockStates) {
+		for (BlockState state : defaultSmoothableBlockStates) {
 			SMOOTHABLE_BLOCK_STATES_CACHE.add(state);
 			tempSmoothableBlockStates.add(state.toString());
 		}
