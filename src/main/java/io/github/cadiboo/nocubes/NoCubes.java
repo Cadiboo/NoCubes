0ed41b8a486e61ed3a09e9f54cb838d882216931
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-02-23 14:09:05 +1100

    Create NoCubes.java

---
+++ src/main/java/io/github/cadiboo/nocubes/NoCubes.java
@@ -0,0 +1,15 @@
+package io.github.cadiboo.nocubes;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static io.github.cadiboo.nocubes.util.ModReference.MOD_ID;
+
+/**
+ * @author Cadiboo
+ */
+@Mod(MOD_ID)
+public final class NoCubes {
+
+
+
+}
