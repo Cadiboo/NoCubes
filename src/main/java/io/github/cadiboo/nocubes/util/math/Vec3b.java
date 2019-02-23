ca2d2e23f1d6c18ed3fa7091cf41841da64ecf61
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-02-23 14:27:40 +1100

    Create Vec3b.java

---
+++ src/main/java/io/github/cadiboo/nocubes/util/math/Vec3b.java
@@ -0,0 +1,65 @@
+package io.github.cadiboo.nocubes.util.math;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+
+/**
+ * Simple pooled 3d byte vector/point with some util methods
+ *
+ * @author Cadiboo
+ */
+public class Vec3b implements Cloneable, AutoCloseable {
+
+//	private static int instances = 0;
+
+	public byte x;
+	public byte y;
+	public byte z;
+
+//	private boolean released;
+
+	private static final ArrayList<Vec3b> POOL = new ArrayList<>();
+
+	private Vec3b(final byte x, final byte y, final byte z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+//		++instances;
+	}
+
+	@Nonnull
+	public static Vec3b retain(final byte x, final byte y, final byte z) {
+		synchronized (POOL) {
+			if (!POOL.isEmpty()) {
+				Vec3b pooled = POOL.remove(POOL.size() - 1);
+				if (pooled != null /*&& pooled.released*/) {
+//					pooled.released = false;
+					pooled.x = x;
+					pooled.y = y;
+					pooled.z = z;
+					return pooled;
+				}
+			}
+		}
+		return new Vec3b(x, y, z);
+	}
+
+	@Override
+	public void close() {
+		synchronized (POOL) {
+			POOL.add(this);
+//			this.released = true;
+		}
+	}
+
+//	static
+//	public int getInstances() {
+//		return instances;
+//	}
+//
+//	static
+//	public int getPoolSize() {
+//		return POOL.size();
+//	}
+
+}
