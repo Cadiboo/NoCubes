c0c60ef57cb06108441455c2f3da7da5c2dd2798
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-03-02 08:24:12 +1100

    Yay, 1.13.2 stuff doesn't exist in 1.12.2, fix ALOAD on integer, stuff works. Good progress on collisions

--- src/main/java/io/github/cadiboo/nocubes/tempcore/Transformer.java
+++ src/main/java/io/github/cadiboo/nocubes/tempcore/Transformer.java
@@ -1,27 +1,19 @@
 package io.github.cadiboo.nocubes.tempcore;
 
 import net.minecraft.launchwrapper.IClassTransformer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.Marker;
-import org.apache.logging.log4j.MarkerManager;
+import net.minecraftforge.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.VarInsnNode;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.HashMap;
 import java.util.ListIterator;
-import java.util.Map;
 import java.util.function.Consumer;
 
 import static net.minecraftforge.fml.relauncher.ReflectionHelper.UnableToFindMethodException;
@@ -33,7 +25,7 @@ public class Transformer implements IClassTransformer, Opcodes {
 
 	@Override
 	public byte[] transform(final String name, final String transformedName, final byte[] basicClass) {
-		if (transformedName.equals("net.minecraft.block.state.IBlockState")) {
+		if (transformedName.equals("net.minecraft.block.state.BlockStateContainer$StateImplementation")) {
 			return transformClass(basicClass, Transformer::redirect_addCollisionBoxToList);
 		}
 
@@ -50,8 +42,8 @@ public class Transformer implements IClassTransformer, Opcodes {
 		return out.toByteArray();
 	}
 
-	private static MethodNode getMethod(ClassNode classNode, String srgName) {
-		final String methodName = ObfuscationHelper.remapName(srgName);
+	private static MethodNode getMethod(ClassNode classNode, String srgName, String methodDescription) {
+		final String methodName = ObfuscationHelper.remapMethodName(classNode.name, srgName, methodDescription);
 		for (final MethodNode method : classNode.methods) {
 			if (method.name.equals(methodName)) {
 				return method;
@@ -66,7 +58,7 @@ public class Transformer implements IClassTransformer, Opcodes {
 
 	public static void redirect_addCollisionBoxToList(final ClassNode classNode) {
 
-		final MethodNode addCollisionBoxToList = getMethod(classNode, "func_185908_a");
+		final MethodNode addCollisionBoxToList = getMethod(classNode, "func_185908_a", "(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/AxisAlignedBB;Ljava/util/List;Lnet/minecraft/entity/Entity;Z)V");
 		final InsnList instructions = addCollisionBoxToList.instructions;
 		final ListIterator<AbstractInsnNode> iterator = instructions.iterator();
 		VarInsnNode ALOAD_0 = null;
@@ -119,7 +111,7 @@ public class Transformer implements IClassTransformer, Opcodes {
 		instructions.insertBefore(ALOAD_0, new VarInsnNode(ALOAD, 3));
 		instructions.insertBefore(ALOAD_0, new VarInsnNode(ALOAD, 4));
 		instructions.insertBefore(ALOAD_0, new VarInsnNode(ALOAD, 5));
-		instructions.insertBefore(ALOAD_0, new VarInsnNode(ALOAD, 6));
+		instructions.insertBefore(ALOAD_0, new VarInsnNode(ILOAD, 6));
 		instructions.insertBefore(ALOAD_0, new MethodInsnNode(
 				INVOKESTATIC,
 				"io/github/cadiboo/nocubes/hooks/Hooks",
@@ -127,6 +119,7 @@ public class Transformer implements IClassTransformer, Opcodes {
 				"(Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/AxisAlignedBB;Ljava/util/List;Lnet/minecraft/entity/Entity;Z)V",
 				false
 		));
+		instructions.insertBefore(ALOAD_0, new InsnNode(RETURN));
 
 	}
 
@@ -134,42 +127,8 @@ public class Transformer implements IClassTransformer, Opcodes {
 
 	private static class ObfuscationHelper {
 
-		private static final Logger LOGGER = LogManager.getLogger();
-		private static final Marker REFLECTION = MarkerManager.getMarker("REFLECTION");
-		private static final Map<String, String> map = new HashMap<>();
-		private static boolean loaded = false;
-
-		public static String remapName(String name) {
-			loadMappings();
-			if (map.isEmpty())
-				return name;
-			return map.getOrDefault(name, name);
-		}
-
-		private static void loadMappings() {
-			if (loaded)
-				return;
-
-			synchronized (map) //Just in case?
-			{
-				if (loaded) //Incase something else loaded while we were here, jump out
-					return;
-				for (String file : new String[]{"fields.csv", "methods.csv"}) {
-					URL path = ClassLoader.getSystemResource(file); //We EXPLICITLY go through the SystemClassLoader here because this is dev-time only. Therefore we will be on the root classpath.
-					if (path == null)
-						continue;
-
-					int count = map.size();
-					LOGGER.info(REFLECTION, "Loading Mappings: {}", path);
-					try (BufferedReader reader = new BufferedReader(new InputStreamReader(path.openStream()))) {
-						reader.lines().skip(1).map(e -> e.split(",")).forEach(e -> map.put(e[0], e[1]));
-					} catch (IOException e1) {
-						LOGGER.error(REFLECTION, "Error reading mappings", e1);
-					}
-					LOGGER.info(REFLECTION, "Loaded {} entries", map.size() - count);
-				}
-				loaded = true;
-			}
+		private static String remapMethodName(String internalClassName, String methodName, String desc) {
+			return FMLDeobfuscatingRemapper.INSTANCE.mapMethodName(internalClassName, methodName, desc);
 		}
 
 	}
