4f14f87889ce22150cbc7d00da1d7e2fe0592197
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-03-01 17:19:34 +1100

    remove old stuff

--- src/main/java/io/github/cadiboo/nocubes/mesh/IMeshGenerator.java
+++ src/main/java/io/github/cadiboo/nocubes/mesh/IMeshGenerator.java
@@ -18,20 +18,5 @@ public interface IMeshGenerator {
 	@Nonnull
 	HashMap<Vec3b, FaceList> generateChunk(final float[] scalarFieldData, final byte[] dimensions);
 
-//	final int[][] cube_vertices = CUBE_VERTICES;
-//	final int[][] tetra_list = TETRA_LIST;
-//
-//	final int[] x = {0, 0, 0};
-//	int n = 0;
-//	final float[] grid = new float[8];
-//	final HashMap<int[], ArrayList<Face<Vec3>>> posToFaces = new HashMap<>();
-//	final ArrayList<Face<Vec3>> faces = new ArrayList<>();
-//
-//	final int[] tempPos = {0, 0, 0};
-//	System.arraycopy(x, 0, tempPos, 0, 3);
-//	posToFaces.put(tempPos, new ArrayList<>(faces));
-//	faces.clear();
-//
-//	return posToFaces;
 
 }
