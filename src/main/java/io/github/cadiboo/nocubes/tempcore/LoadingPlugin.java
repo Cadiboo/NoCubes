aaa2aac357df40383a1dc60e12721c58ddf8769e
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-03-03 18:07:30 +1100

    Update LoadingPlugin.java

--- src/main/java/io/github/cadiboo/nocubes/tempcore/LoadingPlugin.java
+++ src/main/java/io/github/cadiboo/nocubes/tempcore/LoadingPlugin.java
@@ -5,9 +5,16 @@ import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import javax.annotation.Nullable;
 import java.util.Map;
 
+import static net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
+import static net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.SortingIndex;
+import static net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
+
 /**
  * @author Cadiboo
  */
+@TransformerExclusions("io.github.cadiboo.nocubes.tempcore")
+@MCVersion("1.12.2")
+@SortingIndex(Integer.MAX_VALUE - 100)
 public class LoadingPlugin implements IFMLLoadingPlugin {
 
 	@Override
