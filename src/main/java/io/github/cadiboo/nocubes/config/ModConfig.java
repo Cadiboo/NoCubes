8bd8c2ecad006db20edf13189a1ad4a8b5dc9b3b
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-03-04 19:56:58 +1100

    Enable Collisions

--- src/main/java/io/github/cadiboo/nocubes/config/ModConfig.java
+++ src/main/java/io/github/cadiboo/nocubes/config/ModConfig.java
@@ -129,7 +129,7 @@ public final class ModConfig {
 	public static double smoothOtherBlocksAmount = 0.0F;
 
 	@Config.Ignore
-	public static boolean collisionsEnabled = false;
+	public static boolean collisionsEnabled = true;
 
 	@Config.Ignore
 	public static boolean enablePools = true;
