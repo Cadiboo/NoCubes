b76b1fb4d36ea71d1f05b9262e3f4c0daff7ad2f
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-03-31 14:50:37 +1100

    toast :bread:

--- src/main/java/io/github/cadiboo/nocubes/client/gui/toast/BlockStateToast.java
+++ src/main/java/io/github/cadiboo/nocubes/client/gui/toast/BlockStateToast.java
@@ -9,6 +9,9 @@ import net.minecraft.client.gui.toasts.IToast;
 import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.block.model.ItemTransformVec3f;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.client.renderer.vertex.VertexFormatElement;
@@ -19,7 +22,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.common.model.TRSRTransformation;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Vector3f;
 
 import javax.annotation.Nonnull;
 import java.nio.ByteBuffer;
@@ -67,6 +72,7 @@ public abstract class BlockStateToast implements IToast {
 					// Copied from RenderChunk
 					{
 						bufferBuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
+						bufferBuilder.setTranslation((-pos.getX()), (-pos.getY()), (-pos.getZ()));
 					}
 				}
 				// OptiFine Shaders compatibility
@@ -83,77 +89,6 @@ public abstract class BlockStateToast implements IToast {
 				continue;
 			}
 			bufferCache.get(blockRenderLayerId).finishDrawing();
-		}
-
-	}
-
-	public abstract String getUpdateType();
-
-	@Nonnull
-	@Override
-	public Visibility draw(@Nonnull final GuiToast toastGui, final long delta) {
-		final Minecraft minecraft = toastGui.getMinecraft();
-		minecraft.getTextureManager().bindTexture(TEXTURE_TOASTS);
-		GlStateManager.color(1.0F, 1.0F, 1.0F);
-		toastGui.drawTexturedModalRect(0, 0, 0, 0, 160, 32);
-
-		minecraft.fontRenderer.drawString(I18n.format(getUpdateType()) + ":", 30, 7, 0xFFFFFFFF);
-		minecraft.fontRenderer.drawString(name, 30, 18, 0xFFFFFFFF);
-
-		// Code to draw the buffer
-		RENDER:
-		{
-			final Entity entity = minecraft.getRenderViewEntity();
-			if (entity == null) {
-				break RENDER;
-			}
-			final float partialTicks = minecraft.getRenderPartialTicks();
-
-			// Copied from EntityRenderer. This code can be found by looking at usages of Entity.prevPosX.
-			// It also appears in many other places throughout Minecraft's rendering
-			double renderPosX = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * (double) partialTicks;
-			double renderPosY = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * (double) partialTicks;
-			double renderPosZ = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * (double) partialTicks;
-
-			for (int blockRenderLayerId = 0; blockRenderLayerId < usedBlockRenderLayers.length; blockRenderLayerId++) {
-				if (!usedBlockRenderLayers[blockRenderLayerId]) {
-					continue;
-				}
-				final BufferBuilder bufferBuilder = bufferCache.get(blockRenderLayerId);
-				GlStateManager.pushMatrix();
-				//Render everything 1 block higher as an example
-//				GlStateManager.translate(-renderPosX, -renderPosY + 1, -renderPosZ);
-				GlStateManager.scale(10, 10, 0);
-				drawBuffer(bufferBuilder);
-				GlStateManager.popMatrix();
-			}
-		}
-
-		return delta >= 10000L ? Visibility.HIDE : Visibility.SHOW;
-	}
-
-	public static class Add extends BlockStateToast {
-
-		public Add(@Nonnull final IBlockState state, @Nonnull final BlockPos pos, final RayTraceResult result) {
-			super(state, pos, result);
-		}
-
-		@Override
-		public String getUpdateType() {
-			return MOD_ID + ".addedSmoothableBlockState";
-		}
-
-	}
-
-	public static class Remove extends BlockStateToast {
-
-		public Remove(@Nonnull final IBlockState state, @Nonnull final BlockPos pos, final RayTraceResult result) {
-			super(state, pos, result);
-		}
-
-		@Override
-		public String getUpdateType() {
-			return MOD_ID + ".removedSmoothableBlockState";
 		}
 
 	}
@@ -192,6 +127,101 @@ public abstract class BlockStateToast implements IToast {
 
 		//do not reset buffer
 //		bufferBuilderIn.reset();
+	}
+
+	public abstract String getUpdateType();
+
+	@Nonnull
+	@Override
+	public Visibility draw(@Nonnull final GuiToast toastGui, final long delta) {
+		final Minecraft minecraft = toastGui.getMinecraft();
+		minecraft.getTextureManager().bindTexture(TEXTURE_TOASTS);
+		GlStateManager.color(1.0F, 1.0F, 1.0F);
+		toastGui.drawTexturedModalRect(0, 0, 0, 0, 160, 32);
+
+		minecraft.fontRenderer.drawString(I18n.format(getUpdateType()) + ":", 30, 7, 0xFFFFFFFF);
+		minecraft.fontRenderer.drawString(name, 30, 18, 0xFFFFFFFF);
+
+		// Code to draw the buffer
+		RENDER:
+		{
+			final Entity entity = minecraft.getRenderViewEntity();
+			if (entity == null) {
+				break RENDER;
+			}
+			{
+				minecraft.getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+				minecraft.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false);
+
+				GlStateManager.pushMatrix();
+				GlStateManager.translate(0, 0, 100);
+				GlStateManager.enableRescaleNormal();
+				GlStateManager.enableAlpha();
+				GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
+				GlStateManager.enableBlend();
+				GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+				GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+				{
+					GlStateManager.translate(7, 21, 0);
+					GlStateManager.scale(-1, -1, 1);
+					GlStateManager.scale(20, 20, 20);
+				}
+				{
+					GlStateManager.disableLighting();
+					OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240, 240);
+					GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+				}
+				{
+					ForgeHooksClient.multiplyCurrentGlMatrix(
+							new TRSRTransformation(
+									new ItemTransformVec3f(new Vector3f(-30, 225, 0), new Vector3f(), new Vector3f(0.625F, 0.625F, 0.625F))
+							).getMatrix()
+					);
+				}
+			}
+			for (int blockRenderLayerId = 0; blockRenderLayerId < usedBlockRenderLayers.length; blockRenderLayerId++) {
+				if (!usedBlockRenderLayers[blockRenderLayerId]) {
+					continue;
+				}
+				drawBuffer(bufferCache.get(blockRenderLayerId));
+			}
+			{
+				GlStateManager.disableAlpha();
+				GlStateManager.disableRescaleNormal();
+				GlStateManager.popMatrix();
+
+				minecraft.getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+				minecraft.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
+			}
+		}
+
+		return delta >= 10000L ? Visibility.HIDE : Visibility.SHOW;
+	}
+
+	public static class Add extends BlockStateToast {
+
+		public Add(@Nonnull final IBlockState state, @Nonnull final BlockPos pos, final RayTraceResult result) {
+			super(state, pos, result);
+		}
+
+		@Override
+		public String getUpdateType() {
+			return MOD_ID + ".addedSmoothableBlockState";
+		}
+
+	}
+
+	public static class Remove extends BlockStateToast {
+
+		public Remove(@Nonnull final IBlockState state, @Nonnull final BlockPos pos, final RayTraceResult result) {
+			super(state, pos, result);
+		}
+
+		@Override
+		public String getUpdateType() {
+			return MOD_ID + ".removedSmoothableBlockState";
+		}
+
 	}
 
 }
