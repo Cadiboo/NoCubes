c3192b733d42a0a3c78eb176eff5902ef2002c2e
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-10-12 19:19:33 +1100

    :well:

---
+++ src/test/java/io/github/cadiboo/nocubes/client/render/MarchingCubesRenderer.java
@@ -0,0 +1,209 @@
+package io.github.cadiboo.nocubes.client.render;
+
+import io.github.cadiboo.nocubes.client.ClientUtil;
+import io.github.cadiboo.nocubes.client.optifine.OptiFineCompatibility;
+import io.github.cadiboo.nocubes.client.optifine.proxy.OptiFine;
+import io.github.cadiboo.nocubes.hooks.Hooks;
+import io.github.cadiboo.nocubes.util.pooled.Vec3;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.RegionRenderCacheBuilder;
+import net.minecraft.client.renderer.chunk.ChunkRender;
+import net.minecraft.client.renderer.chunk.ChunkRenderTask;
+import net.minecraft.client.renderer.chunk.CompiledChunk;
+import net.minecraft.fluid.IFluidState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.PooledMutableBlockPos;
+import net.minecraft.world.IEnviromentBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.model.data.IModelData;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
+
+import static io.github.cadiboo.nocubes.client.ClientUtil.BLOCK_RENDER_LAYER_VALUES;
+import static io.github.cadiboo.nocubes.client.ClientUtil.BLOCK_RENDER_LAYER_VALUES_LENGTH;
+import static io.github.cadiboo.nocubes.mesh.generator.MarchingCubes.CUBE_VERTS;
+import static io.github.cadiboo.nocubes.mesh.generator.MarchingCubes.EDGE_TABLE;
+
+/**
+ * @author Cadiboo
+ */
+public class MarchingCubesRenderer {
+
+	public static void renderChunk(
+			@Nonnull final ChunkRender chunkRender,
+			@Nonnull final BlockPos chunkRenderPos,
+			@Nonnull final ChunkRenderTask chunkRenderTask,
+			@Nonnull final CompiledChunk compiledChunk,
+			// Use World for eagerly generated caches
+			@Nonnull final IWorld world,
+			// Use RenderChunkCache for lazily generated caches
+			@Nonnull final IEnviromentBlockReader chunkRenderCache,
+			@Nonnull final boolean[] usedBlockRenderLayers,
+			@Nonnull final Random random,
+			@Nonnull final BlockRendererDispatcher blockRendererDispatcher
+	) {
+		final int posX = chunkRenderPos.getX();
+		final int posY = chunkRenderPos.getY();
+		final int posZ = chunkRenderPos.getZ();
+
+		final RegionRenderCacheBuilder builders = chunkRenderTask.getRegionRenderCacheBuilder();
+		final OptiFine optiFine = OptiFineCompatibility.get();
+
+		try (PooledMutableBlockPos pos = PooledMutableBlockPos.retain()) {
+			for (int z = 0; z < 16; ++z) {
+				for (int y = 0; y < 16; ++y) {
+					for (int x = 0; x < 16; ++x) {
+						pos.setPos(posX + x, posY + y, posZ + z);
+
+						final BlockState blockState = chunkRenderCache.getBlockState(pos);
+						final IFluidState fluidState = chunkRenderCache.getFluidState(pos);
+
+						boolean forgeRenderLayerChanged = false;
+						IModelData modelData = null;
+
+						for (int i = BLOCK_RENDER_LAYER_VALUES_LENGTH - 1; i >= 0; i--) {
+							final BlockRenderLayer initial = BLOCK_RENDER_LAYER_VALUES[i];
+
+							final boolean renderFluid = !fluidState.isEmpty() && fluidState.canRenderInLayer(initial);
+							final boolean renderBlock = !Hooks.canBlockStateRender(blockState) && blockState.getRenderType() != BlockRenderType.INVISIBLE;
+
+							int layerOrdinal = 0;
+							BlockRenderLayer layer = null;
+							BufferBuilder bufferBuilder = null;
+
+							if (renderFluid || renderBlock) {
+								layerOrdinal = ClientUtil.getCorrectRenderLayer(i);
+								layer = BLOCK_RENDER_LAYER_VALUES[layerOrdinal];
+								ForgeHooksClient.setRenderLayer(layer);
+								forgeRenderLayerChanged = true;
+								bufferBuilder = builders.getBuilder(layerOrdinal);
+								if (modelData == null)
+									modelData = chunkRenderTask.getModelData(pos);
+							}
+							if (renderFluid) {
+//								Object renderEnv = optiFine.preRenderBlock(bufferBuilder, layer, fluidState, pos, builders, chunkRenderCache);
+								Object renderEnv = optiFine.preRenderBlock(bufferBuilder, layer, blockState, pos, builders, chunkRenderCache);
+								ClientUtil.startOrContinueBufferBuilder(compiledChunk, layer, chunkRender, chunkRenderPos, bufferBuilder);
+								usedBlockRenderLayers[layerOrdinal] |= blockRendererDispatcher.renderFluid(pos, chunkRenderCache, bufferBuilder, fluidState);
+								optiFine.postRenderBlock(renderEnv, chunkRender, builders, compiledChunk, usedBlockRenderLayers);
+							}
+							if (renderBlock) {
+								Object renderEnv = optiFine.preRenderBlock(bufferBuilder, layer, blockState, pos, builders, chunkRenderCache);
+								ClientUtil.startOrContinueBufferBuilder(compiledChunk, layer, chunkRender, chunkRenderPos, bufferBuilder);
+//								usedBlockRenderLayers[layerOrdinal] |= blockRendererDispatcher.renderBlock(blockState, pos, chunkRenderCache, bufferBuilder, random, modelData);
+								usedBlockRenderLayers[layerOrdinal] |= render(chunkRender, chunkRenderPos, chunkRenderTask, compiledChunk, world, chunkRenderCache, usedBlockRenderLayers, random, blockRendererDispatcher, builders, pos, layer, bufferBuilder, modelData, renderEnv);
+								optiFine.postRenderBlock(renderEnv, chunkRender, builders, compiledChunk, usedBlockRenderLayers);
+							}
+						}
+						if (forgeRenderLayerChanged)
+							ForgeHooksClient.setRenderLayer(null);
+					}
+				}
+			}
+		}
+	}
+
+	private static boolean render(final ChunkRender chunkRender, final BlockPos chunkRenderPos, final ChunkRenderTask chunkRenderTask, final CompiledChunk compiledChunk, final IWorld world, final IEnviromentBlockReader chunkRenderCache, final boolean[] usedBlockRenderLayers, final Random random, final BlockRendererDispatcher blockRendererDispatcher, final RegionRenderCacheBuilder builders, final PooledMutableBlockPos pos, final BlockRenderLayer layer, final BufferBuilder bufferBuilder, final IModelData modelData, final Object renderEnv) {
+		float isolevel = 0.5F;
+
+		Vec3[] grid_p = new Vec3[8];
+
+		float[] grid = new float[8];
+		for (int i = 0; i < CUBE_VERTS.length; i++) {
+			final byte[] offset = CUBE_VERTS[i];
+
+			final BlockPos pos1 = pos.add(offset[0], offset[1], offset[2]);
+
+			grid_p[i] = Vec3.retain(pos.getX(), pos.getY(), pos.getZ());
+
+			final BlockState state = chunkRenderCache.getBlockState(pos1);
+			grid[i] = state.nocubes_isTerrainSmoothable ? 0 : 1;
+		}
+
+		// Determine the index into the edge table which tells us which vertices are inside of the surface
+		int cubeindex = 0;
+		if (grid[0] < isolevel) cubeindex |= 1;
+		if (grid[1] < isolevel) cubeindex |= 2;
+		if (grid[2] < isolevel) cubeindex |= 4;
+		if (grid[3] < isolevel) cubeindex |= 8;
+		if (grid[4] < isolevel) cubeindex |= 16;
+		if (grid[5] < isolevel) cubeindex |= 32;
+		if (grid[6] < isolevel) cubeindex |= 64;
+		if (grid[7] < isolevel) cubeindex |= 128;
+
+		if (EDGE_TABLE[cubeindex] == 0)
+			return false; //Cube is entirely in/out of the surface
+
+		Vec3[] vertlist = new Vec3[12];
+
+		// Find the vertices where the surface intersects the cube
+		if ((EDGE_TABLE[cubeindex] & 1) != 0)
+			vertlist[0] =
+					vertexInterp(isolevel, grid_p[0], grid_p[1], grid[0], grid[1]);
+		if ((EDGE_TABLE[cubeindex] & 2) != 0)
+			vertlist[1] =
+					vertexInterp(isolevel, grid_p[1], grid_p[2], grid[1], grid[2]);
+		if ((EDGE_TABLE[cubeindex] & 4) != 0)
+			vertlist[2] =
+					vertexInterp(isolevel, grid_p[2], grid_p[3], grid[2], grid[3]);
+		if ((EDGE_TABLE[cubeindex] & 8) != 0)
+			vertlist[3] =
+					vertexInterp(isolevel, grid_p[3], grid_p[0], grid[3], grid[0]);
+		if ((EDGE_TABLE[cubeindex] & 16) != 0)
+			vertlist[4] =
+					vertexInterp(isolevel, grid_p[4], grid_p[5], grid[4], grid[5]);
+		if ((EDGE_TABLE[cubeindex] & 32) != 0)
+			vertlist[5] =
+					vertexInterp(isolevel, grid_p[5], grid_p[6], grid[5], grid[6]);
+		if ((EDGE_TABLE[cubeindex] & 64) != 0)
+			vertlist[6] =
+					vertexInterp(isolevel, grid_p[6], grid_p[7], grid[6], grid[7]);
+		if ((EDGE_TABLE[cubeindex] & 128) != 0)
+			vertlist[7] =
+					vertexInterp(isolevel, grid_p[7], grid_p[4], grid[7], grid[4]);
+		if ((EDGE_TABLE[cubeindex] & 256) != 0)
+			vertlist[8] =
+					vertexInterp(isolevel, grid_p[0], grid_p[4], grid[0], grid[4]);
+		if ((EDGE_TABLE[cubeindex] & 512) != 0)
+			vertlist[9] =
+					vertexInterp(isolevel, grid_p[1], grid_p[5], grid[1], grid[5]);
+		if ((EDGE_TABLE[cubeindex] & 1024) != 0)
+			vertlist[10] =
+					vertexInterp(isolevel, grid_p[2], grid_p[6], grid[2], grid[6]);
+		if ((EDGE_TABLE[cubeindex] & 2048) != 0)
+			vertlist[11] =
+					vertexInterp(isolevel, grid_p[3], grid_p[7], grid[3], grid[7]);
+
+		// I don't know why I do this
+		// or what to do here
+
+		return true;
+	}
+
+	/**
+	 * Linearly interpolate the position where an isosurface cuts
+	 * an edge between two vertices, each with their own scalar value
+	 */
+	private static Vec3 vertexInterp(double isolevel, Vec3 p1, Vec3 p2, double valp1, double valp2) {
+		if (Math.abs(isolevel - valp1) < 0.00001)
+			return (p1);
+		if (Math.abs(isolevel - valp2) < 0.00001)
+			return (p2);
+		if (Math.abs(valp1 - valp2) < 0.00001)
+			return (p1);
+		double mu = (isolevel - valp1) / (valp2 - valp1);
+		Vec3 p = Vec3.retain(0, 0, 0);
+		p.x = p1.x + mu * (p2.x - p1.x);
+		p.y = p1.y + mu * (p2.y - p1.y);
+		p.z = p1.z + mu * (p2.z - p1.z);
+
+		return p;
+	}
+
+}
---
+++ src/test/java/io/github/cadiboo/nocubes/client/render/NewRenderDispatcher.java
@@ -0,0 +1,137 @@
+package io.github.cadiboo.nocubes.client.render;
+
+import io.github.cadiboo.nocubes.client.ClientUtil;
+import io.github.cadiboo.nocubes.client.optifine.OptiFineCompatibility;
+import io.github.cadiboo.nocubes.client.optifine.proxy.OptiFine;
+import io.github.cadiboo.nocubes.hooks.Hooks;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.RegionRenderCacheBuilder;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.chunk.ChunkRender;
+import net.minecraft.client.renderer.chunk.ChunkRenderTask;
+import net.minecraft.client.renderer.chunk.CompiledChunk;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.fluid.IFluidState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.PooledMutableBlockPos;
+import net.minecraft.world.IEnviromentBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.model.data.IModelData;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
+
+import static io.github.cadiboo.nocubes.client.ClientUtil.BLOCK_RENDER_LAYER_VALUES;
+import static io.github.cadiboo.nocubes.client.ClientUtil.BLOCK_RENDER_LAYER_VALUES_LENGTH;
+
+/**
+ * @author Cadiboo
+ */
+public final class NewRenderDispatcher {
+
+	public static void renderChunkVanilla(
+			@Nonnull final ChunkRender chunkRender,
+			@Nonnull final BlockPos chunkRenderPos,
+			@Nonnull final ChunkRenderTask chunkRenderTask,
+			@Nonnull final CompiledChunk compiledChunk,
+			// Use World for eagerly generated caches
+			@Nonnull final IWorld world,
+			// Use RenderChunkCache for lazily generated caches
+			@Nonnull final IEnviromentBlockReader chunkRenderCache,
+			@Nonnull final boolean[] usedBlockRenderLayers,
+			@Nonnull final Random random,
+			@Nonnull final BlockRendererDispatcher blockRendererDispatcher
+	) {
+		final int posX = chunkRenderPos.getX();
+		final int posY = chunkRenderPos.getY();
+		final int posZ = chunkRenderPos.getZ();
+
+		final RegionRenderCacheBuilder builders = chunkRenderTask.getRegionRenderCacheBuilder();
+		final OptiFine optiFine = OptiFineCompatibility.get();
+
+		try (PooledMutableBlockPos pos = PooledMutableBlockPos.retain()) {
+			for (int z = 0; z < 16; ++z) {
+				for (int y = 0; y < 16; ++y) {
+					for (int x = 0; x < 16; ++x) {
+						pos.setPos(posX + x, posY + y, posZ + z);
+
+						final BlockState blockState = chunkRenderCache.getBlockState(pos);
+						final IFluidState fluidState = chunkRenderCache.getFluidState(pos);
+
+						boolean forgeRenderLayerChanged = false;
+						IModelData modelData = null;
+
+						for (int i = BLOCK_RENDER_LAYER_VALUES_LENGTH - 1; i >= 0; i--) {
+							final BlockRenderLayer initial = BLOCK_RENDER_LAYER_VALUES[i];
+
+							final boolean renderFluid = !fluidState.isEmpty() && fluidState.canRenderInLayer(initial);
+							final boolean renderBlock = !Hooks.canBlockStateRender(blockState) && blockState.getRenderType() != BlockRenderType.INVISIBLE;
+
+							int layerOrdinal = 0;
+							BlockRenderLayer layer = null;
+							BufferBuilder bufferBuilder = null;
+
+							if (renderFluid || renderBlock) {
+								layerOrdinal = ClientUtil.getCorrectRenderLayer(i);
+								layer = BLOCK_RENDER_LAYER_VALUES[layerOrdinal];
+								ForgeHooksClient.setRenderLayer(layer);
+								forgeRenderLayerChanged = true;
+								bufferBuilder = builders.getBuilder(layerOrdinal);
+								if (modelData == null)
+									modelData = chunkRenderTask.getModelData(pos);
+							}
+							if (renderFluid) {
+//								Object renderEnv = optiFine.preRenderBlock(bufferBuilder, layer, fluidState, pos, builders, chunkRenderCache);
+								Object renderEnv = optiFine.preRenderBlock(bufferBuilder, layer, blockState, pos, builders, chunkRenderCache);
+								ClientUtil.startOrContinueBufferBuilder(compiledChunk, layer, chunkRender, chunkRenderPos, bufferBuilder);
+								usedBlockRenderLayers[layerOrdinal] |= blockRendererDispatcher.renderFluid(pos, chunkRenderCache, bufferBuilder, fluidState);
+								optiFine.postRenderBlock(renderEnv, chunkRender, builders, compiledChunk, usedBlockRenderLayers);
+							}
+							if (renderBlock) {
+								Object renderEnv = optiFine.preRenderBlock(bufferBuilder, layer, blockState, pos, builders, chunkRenderCache);
+								ClientUtil.startOrContinueBufferBuilder(compiledChunk, layer, chunkRender, chunkRenderPos, bufferBuilder);
+								usedBlockRenderLayers[layerOrdinal] |= blockRendererDispatcher.renderBlock(blockState, pos, chunkRenderCache, bufferBuilder, random, modelData);
+								optiFine.postRenderBlock(renderEnv, chunkRender, builders, compiledChunk, usedBlockRenderLayers);
+							}
+						}
+						if (forgeRenderLayerChanged)
+							ForgeHooksClient.setRenderLayer(null);
+					}
+				}
+			}
+		}
+	}
+
+	public static void renderChunk(
+			@Nonnull final ChunkRender chunkRender,
+			@Nonnull final BlockPos chunkRenderPos,
+			@Nonnull final ChunkRenderTask chunkRenderTask,
+			@Nonnull final CompiledChunk compiledChunk,
+			// Use World for eagerly generated caches
+			@Nonnull final IWorld world,
+			// Use RenderChunkCache for lazily generated caches
+			@Nonnull final IEnviromentBlockReader chunkRenderCache,
+			@Nonnull final boolean[] usedBlockRenderLayers,
+			@Nonnull final Random random,
+			@Nonnull final BlockRendererDispatcher blockRendererDispatcher
+	) {
+		MarchingCubesRenderer.renderChunk(chunkRender, chunkRenderPos, chunkRenderTask, compiledChunk, world, chunkRenderCache, usedBlockRenderLayers, random, blockRendererDispatcher);
+	}
+
+	public static void renderSmoothBlockDamage(
+			final Tessellator tessellatorIn,
+			final BufferBuilder bufferBuilderIn,
+			final BlockPos blockpos,
+			final BlockState iblockstate,
+			final IEnviromentBlockReader world,
+			final TextureAtlasSprite textureatlassprite
+	) {
+
+	}
+
+}
