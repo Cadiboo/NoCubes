d59adbaf98b16da20fab970f9dc2f1f074da8f27
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2020-04-05 10:40:59 +1000

    Update TestUtil.java

--- src/test/java/io/github/cadiboo/nocubes/test/TestUtil.java
+++ src/test/java/io/github/cadiboo/nocubes/test/TestUtil.java
@@ -8,6 +8,13 @@ import net.minecraftforge.common.util.Lazy;
  */
 public final class TestUtil {
 
+	public static final Lazy<Boolean> IS_CI_ENVIRONMENT = Lazy.concurrentOf(() -> Boolean.parseBoolean(System.getenv("CI")) ||
+		Boolean.parseBoolean(System.getenv("CONTINUOUS_INTEGRATION")) ||
+		Integer.parseInt(System.getenv("BUILD_NUMBER")) > 0 ||
+		Integer.parseInt(System.getenv("TRAVIS_BUILD_NUMBER")) > 0 ||
+		Integer.parseInt(System.getenv("CIRCLE_BUILD_NUM")) > 0
+	);
+
 	public static void assertFalse(final boolean b) {
 		assertTrue(!b);
 	}
@@ -15,7 +22,5 @@ public final class TestUtil {
 	public static void assertTrue(final boolean b) {
 		Preconditions.checkArgument(b);
 	}
-
-	public static final Lazy<Boolean> IS_CI_ENVIRONMENT = Lazy.concurrentOf(() -> Boolean.parseBoolean(System.getenv("BUILD_NUMBER")) || Boolean.parseBoolean(System.getenv("TRAVIS_BUILD_NUMBER")) || Boolean.parseBoolean(System.getenv("CIRCLE_BUILD_NUM")));
 
 }
