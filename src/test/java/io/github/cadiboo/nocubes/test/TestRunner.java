6e02ec7511b1b3ce9904ffc368408597a8ea00ab
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2020-04-05 10:12:27 +1000

    Testing

--- src/test/java/io/github/cadiboo/nocubes/test/TestRunner.java
+++ src/test/java/io/github/cadiboo/nocubes/test/TestRunner.java
@@ -1,12 +1,11 @@
 package io.github.cadiboo.nocubes.test;
 
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Cadiboo
@@ -17,27 +16,34 @@ public final class TestRunner {
 	@SubscribeEvent
 	public static void runTests(FMLServerStartedEvent event) {
 		final TestRepository testRepository = new TestRepository();
-		final AtomicInteger fails = new AtomicInteger(0);
-		testRepository.tests.forEach(test -> {
-			try {
-				test.action.run();
-			} catch (Exception e) {
-				event.getServer().sendMessage(new StringTextComponent("TEST FAILED: " + test.name).applyTextStyle(TextFormatting.RED));
-				e.printStackTrace();
-				fails.getAndIncrement();
-			}
-		});
-		if (fails.get() > 0)
-			event.getServer().sendMessage(new StringTextComponent(fails.get() + " TESTS FAILED").applyTextStyle(TextFormatting.RED));
+		final MinecraftServer server = event.getServer();
+		final long fails = testRepository.tests.parallelStream()
+			.filter(test -> runTestWithCatch(test, server))
+			.count();
+		if (fails > 0)
+			server.sendMessage(new StringTextComponent(fails + " TESTS FAILED").applyTextStyle(TextFormatting.RED));
 		else
-			event.getServer().sendMessage(new StringTextComponent("ALL TESTS PASSED").applyTextStyle(TextFormatting.GREEN));
-//		if (ModUtil.IS_DEVELOPER_WORKSPACE.get())
-//			return;
-		// Assuming CI
-		if (fails.get() > 0)
-			throw new RuntimeException("had failed tests");
+			server.sendMessage(new StringTextComponent("ALL TESTS PASSED").applyTextStyle(TextFormatting.GREEN));
+		if (!TestUtil.IS_CI_ENVIRONMENT.get())
+			return;
+		if (fails > 0)
+			throw new RuntimeException("Had failed tests");
 		else
 			event.getServer().initiateShutdown(false);
+	}
+
+	/**
+	 * @return if the test FAILED
+	 */
+	private static boolean runTestWithCatch(final Test test, final MinecraftServer server) {
+		try {
+			test.action.run();
+		} catch (Exception e) {
+			server.sendMessage(new StringTextComponent("TEST FAILED: " + test.name).applyTextStyle(TextFormatting.RED));
+			e.printStackTrace();
+			return true;
+		}
+		return false;
 	}
 
 }
--- src/test/java/io/github/cadiboo/nocubes/test/TestUtil.java
+++ src/test/java/io/github/cadiboo/nocubes/test/TestUtil.java
@@ -1,6 +1,7 @@
 package io.github.cadiboo.nocubes.test;
 
 import com.google.common.base.Preconditions;
+import net.minecraftforge.common.util.Lazy;
 
 /**
  * @author Cadiboo
@@ -14,5 +15,7 @@ public final class TestUtil {
 	public static void assertTrue(final boolean b) {
 		Preconditions.checkArgument(b);
 	}
+
+	public static final Lazy<Boolean> IS_CI_ENVIRONMENT = Lazy.concurrentOf(() -> Boolean.parseBoolean(System.getenv("BUILD_NUMBER")) || Boolean.parseBoolean(System.getenv("TRAVIS_BUILD_NUMBER")) || Boolean.parseBoolean(System.getenv("CIRCLE_BUILD_NUM")));
 
 }
