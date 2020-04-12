1633f1631965ff185bda896b4b277e7497d704f6
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2020-04-12 18:35:27 +1000

    Logging

--- src/test/java/io/github/cadiboo/nocubes/test/TestRunner.java
+++ src/test/java/io/github/cadiboo/nocubes/test/TestRunner.java
@@ -1,17 +1,22 @@
 package io.github.cadiboo.nocubes.test;
 
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author Cadiboo
  */
 @Mod.EventBusSubscriber
 public final class TestRunner {
+
+	private static final Logger LOGGER = LogManager.getLogger();
 
 	@SubscribeEvent
 	public static void runTests(FMLServerStartedEvent event) {
@@ -21,15 +26,20 @@ public final class TestRunner {
 			.filter(test -> runTestWithCatch(test, server))
 			.count();
 		if (fails > 0)
-			server.sendMessage(new StringTextComponent(fails + " TESTS FAILED").applyTextStyle(TextFormatting.RED));
+			log(server, new StringTextComponent(fails + " TESTS FAILED").applyTextStyle(TextFormatting.RED));
 		else
-			server.sendMessage(new StringTextComponent("ALL TESTS PASSED").applyTextStyle(TextFormatting.GREEN));
+			log(server, new StringTextComponent("ALL TESTS PASSED").applyTextStyle(TextFormatting.GREEN));
 		if (!TestUtil.IS_CI_ENVIRONMENT.get())
 			return;
 		if (fails > 0)
 			throw new RuntimeException("Had failed tests");
 		else
 			event.getServer().initiateShutdown(false);
+	}
+
+	private static void log(final MinecraftServer server, final ITextComponent component) {
+		server.sendMessage(component);
+		LOGGER.info(component.getFormattedText());
 	}
 
 	/**
@@ -39,7 +49,7 @@ public final class TestRunner {
 		try {
 			test.action.run();
 		} catch (Exception e) {
-			server.sendMessage(new StringTextComponent("TEST FAILED: " + test.name).applyTextStyle(TextFormatting.RED));
+			log(server, new StringTextComponent("TEST FAILED: " + test.name).applyTextStyle(TextFormatting.RED));
 			e.printStackTrace();
 			return true;
 		}
