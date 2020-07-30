5f0982ca153670ecf5f60117d419c37a0f228ca5
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2020-07-30 12:02:55 +1000

    Use Mockito

--- src/test/java/io/github/cadiboo/nocubes/TestUtils.java
+++ src/test/java/io/github/cadiboo/nocubes/TestUtils.java
@@ -1,13 +1,13 @@
 package io.github.cadiboo.nocubes;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.block.TestBlockState;
+import org.mockito.Mockito;
 
 /**
  * @author Cadiboo
  */
 public class TestUtils {
 
-	public static final BlockState TEST_1 = TestBlockState.create();
+	public static final BlockState TEST_1 = Mockito.mock(BlockState.class);
 
 }
