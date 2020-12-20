f5c86750a1f61099af0f66e118025800002fb69e
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2020-12-20 18:12:16 +1100

    Extract lookup tables

--- src/main/java/io/github/cadiboo/nocubes/mesh/SurfaceNets.java
+++ src/main/java/io/github/cadiboo/nocubes/mesh/SurfaceNets.java
@@ -8,10 +8,12 @@ import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.gen.WorldGenRegion;
 
 import java.util.ArrayList;
 import java.util.function.Predicate;
+
+import static io.github.cadiboo.nocubes.mesh.SurfaceNets.Lookup.CUBE_EDGES;
+import static io.github.cadiboo.nocubes.mesh.SurfaceNets.Lookup.EDGE_TABLE;
 
 /**
  * Written by Mikola Lysenko (C) 2012
@@ -24,76 +26,6 @@ public class SurfaceNets {
 	public static final int MESH_SIZE_POSITIVE_EXTENSION = 1;
 	// Seams appear in the meshes, surface nets generates a mesh 1 smaller than it "should"
 	public static final int MESH_SIZE_NEGATIVE_EXTENSION = 1;
-
-	/**
-	 * A list of vertices where x/y/z are represented as bits (either 0 or 1)
-	 * E.g. (1, 0, 1) -> 101
-	 * I think it also has some extra info
-	 */
-	public static final int[] CUBE_EDGES = new int[24];
-	public static final int[] EDGE_TABLE = new int[256];
-
-	// because the tables are so big we compute them in a static {} instead of hardcoding them (I think)
-	static {
-		generateCubeEdgesTable();
-		generateIntersectionTable();
-	}
-
-	/**
-	 * Utility function to build a table of possible edges for a cube with each
-	 * pair of points representing one edge i.e. [0,1,0,2,0,4,...] would be the
-	 * edges from points 0 to 1, 0 to 2, and 0 to 4 respectively:
-	 *
-	 * <pre>
-	 *  y         z
-	 *  ^        /
-	 *  |
-	 *    6----7
-	 *   /|   /|
-	 *  4----5 |
-	 *  | 2--|-3
-	 *  |/   |/
-	 *  0----1   --> x
-	 * </pre>
-	 */
-	private static void generateCubeEdgesTable() {
-		//Initialize the cube_edges table
-		// This is just the vertex number (number of corners) of each cube
-		int cubeEdgesIndex = 0;
-		// 8 is the number of corners for a cube
-		for (byte cubeCornerIndex = 0; cubeCornerIndex < 8; ++cubeCornerIndex) {
-			for (int em = 1; em <= 4; em <<= 1) {
-				int j = cubeCornerIndex ^ em;
-				if (cubeCornerIndex <= j) {
-					CUBE_EDGES[cubeEdgesIndex++] = cubeCornerIndex;
-					CUBE_EDGES[cubeEdgesIndex++] = j;
-				}
-			}
-		}
-	}
-
-	/**
-	 * Build an intersection table. This is a 2^(cube config) -> 2^(edge config) map
-	 * There is only one entry for each possible cube configuration
-	 * and the output is a 12-bit vector enumerating all edges
-	 * crossing the 0-level
-	 */
-	private static void generateIntersectionTable() {
-		// nope, I don't understand this either
-		// yay, Lookup Tables...
-		// Initialize the intersection table.
-		// This is a 2^(cube configuration) ->  2^(edge configuration) map
-		// There is one entry for each possible cube configuration, and the output is a 12-bit vector enumerating all edges crossing the 0-level.
-		for (short edgeTableIndex = 0; edgeTableIndex < 256; ++edgeTableIndex) {
-			short em = 0;
-			for (int cubeEdgesIndex = 0; cubeEdgesIndex < 24; cubeEdgesIndex += 2) {
-				final boolean a = (edgeTableIndex & (1 << CUBE_EDGES[cubeEdgesIndex])) != 0;
-				final boolean b = (edgeTableIndex & (1 << CUBE_EDGES[cubeEdgesIndex + 1])) != 0;
-				em |= a != b ? 1 << (cubeEdgesIndex >> 1) : 0;
-			}
-			EDGE_TABLE[edgeTableIndex] = em;
-		}
-	}
 
 	public static void generate(
 		int startX, int startY, int startZ,
@@ -302,6 +234,80 @@ public class SurfaceNets {
 	public interface MeshAction {
 
 		boolean apply(BlockPos.Mutable pos, Face face);
+
+	}
+
+	static final class Lookup {
+
+		/**
+		 * A list of vertices where x/y/z are represented as bits (either 0 or 1)
+		 * E.g. (1, 0, 1) -> 101
+		 * I think it also has some extra info
+		 */
+		public static final int[] CUBE_EDGES = new int[24];
+		public static final int[] EDGE_TABLE = new int[256];
+
+		// because the tables are so big we compute them in a static {} instead of hardcoding them (I think)
+		static {
+			generateCubeEdgesTable();
+			generateIntersectionTable();
+		}
+
+		/**
+		 * Utility function to build a table of possible edges for a cube with each
+		 * pair of points representing one edge i.e. [0,1,0,2,0,4,...] would be the
+		 * edges from points 0 to 1, 0 to 2, and 0 to 4 respectively:
+		 *
+		 * <pre>
+		 *  y         z
+		 *  ^        /
+		 *  |
+		 *    6----7
+		 *   /|   /|
+		 *  4----5 |
+		 *  | 2--|-3
+		 *  |/   |/
+		 *  0----1   --> x
+		 * </pre>
+		 */
+		private static void generateCubeEdgesTable() {
+			//Initialize the cube_edges table
+			// This is just the vertex number (number of corners) of each cube
+			int cubeEdgesIndex = 0;
+			// 8 is the number of corners for a cube
+			for (byte cubeCornerIndex = 0; cubeCornerIndex < 8; ++cubeCornerIndex) {
+				for (int em = 1; em <= 4; em <<= 1) {
+					int j = cubeCornerIndex ^ em;
+					if (cubeCornerIndex <= j) {
+						CUBE_EDGES[cubeEdgesIndex++] = cubeCornerIndex;
+						CUBE_EDGES[cubeEdgesIndex++] = j;
+					}
+				}
+			}
+		}
+
+		/**
+		 * Build an intersection table. This is a 2^(cube config) -> 2^(edge config) map
+		 * There is only one entry for each possible cube configuration
+		 * and the output is a 12-bit vector enumerating all edges
+		 * crossing the 0-level
+		 */
+		private static void generateIntersectionTable() {
+			// nope, I don't understand this either
+			// yay, Lookup Tables...
+			// Initialize the intersection table.
+			// This is a 2^(cube configuration) ->  2^(edge configuration) map
+			// There is one entry for each possible cube configuration, and the output is a 12-bit vector enumerating all edges crossing the 0-level.
+			for (short edgeTableIndex = 0; edgeTableIndex < 256; ++edgeTableIndex) {
+				short em = 0;
+				for (int cubeEdgesIndex = 0; cubeEdgesIndex < 24; cubeEdgesIndex += 2) {
+					final boolean a = (edgeTableIndex & (1 << CUBE_EDGES[cubeEdgesIndex])) != 0;
+					final boolean b = (edgeTableIndex & (1 << CUBE_EDGES[cubeEdgesIndex + 1])) != 0;
+					em |= a != b ? 1 << (cubeEdgesIndex >> 1) : 0;
+				}
+				EDGE_TABLE[edgeTableIndex] = em;
+			}
+		}
 
 	}
 
