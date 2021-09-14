ecfa8a5200a183b1e697bca422e8b819c74b4ac8
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2021-09-15 00:24:02 +1000

    Clean up ClientEventSubscriber and fix server collision disabling only working in debug mode

--- src/main/java/io/github/cadiboo/nocubes/client/ClientEventSubscriber.java
+++ src/main/java/io/github/cadiboo/nocubes/client/ClientEventSubscriber.java
@@ -4,13 +4,12 @@ import io.github.cadiboo.nocubes.NoCubes;
 import io.github.cadiboo.nocubes.config.NoCubesConfig;
 import io.github.cadiboo.nocubes.hooks.SelfCheck;
 import io.github.cadiboo.nocubes.network.NoCubesNetwork;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.Util;
 import net.minecraft.ChatFormatting;
+import net.minecraft.Util;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -26,31 +25,30 @@ public final class ClientEventSubscriber {
 	private static long selfCheckInfoPrintedAt = Long.MIN_VALUE;
 
 	@SubscribeEvent
-	public static void onTick(TickEvent.ClientTickEvent event) {
-		if (!NoCubesConfig.Client.debugEnabled)
-			return;
-
-		Minecraft minecraft = Minecraft.getInstance();
-		Level world = minecraft.level;
-		if (world == null)
-			return;
-
-		if (Screen.hasAltDown()) {
-			long time = world.getGameTime();
-			// Only print once every 10 seconds, don't spam the log
-			if (time - 10 * 20 > selfCheckInfoPrintedAt) {
-				selfCheckInfoPrintedAt = time;
-				LogManager.getLogger("NoCubes Hooks SelfCheck").debug(String.join("\n", SelfCheck.info()));
-			}
-		}
-
-		LocalPlayer player = Minecraft.getInstance().player;
-		if (player == null)
-			return;
+	public static void onClientTick(TickEvent.ClientTickEvent event) {
+		if (NoCubesConfig.Client.debugEnabled && Screen.hasAltDown())
+			printDebugInfo();
 
 		if (NoCubesConfig.Server.collisionsEnabled && !NoCubesNetwork.currentServerHasNoCubes) {
-			player.sendMessage(new TranslatableComponent(NoCubes.MOD_ID + ".notification.nocubesNotInstalledOnServerCollisionsUnavailable").withStyle(ChatFormatting.RED), Util.NIL_UUID);
 			NoCubesConfig.Server.collisionsEnabled = false;
+			String msg_key = NoCubes.MOD_ID + ".notification.nocubesNotInstalledOnServerCollisionsUnavailable";
+			var player = Minecraft.getInstance().player;
+			if (player == null)
+				LogManager.getLogger("NoCubes notification fallback").warn(I18n.get(msg_key));
+			else
+				player.sendMessage(new TranslatableComponent(msg_key).withStyle(ChatFormatting.RED), Util.NIL_UUID);
+		}
+	}
+
+	private static void printDebugInfo() {
+		var world = Minecraft.getInstance().level;
+		if (world == null)
+			return;
+		long time = world.getGameTime();
+		// Only print once every 10 seconds, don't spam the log
+		if (time - 10 * 20 > selfCheckInfoPrintedAt) {
+			selfCheckInfoPrintedAt = time;
+			LogManager.getLogger("NoCubes Hooks SelfCheck").debug(String.join("\n", SelfCheck.info()));
 		}
 	}
 
