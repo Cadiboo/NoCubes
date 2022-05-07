b214c35d801787e1150f7de83bfc6e94e4c640e6
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2022-05-07 23:27:56 +1000

    Add reference to original source

--- src/main/java/io/github/cadiboo/nocubes/hooks/INoCubesBlockState.java
+++ src/main/java/io/github/cadiboo/nocubes/hooks/INoCubesBlockState.java
@@ -1,5 +1,9 @@
 package io.github.cadiboo.nocubes.hooks;
 
+/**
+ * Implemented (by Mixin) on BlockStateBase.
+ * Inspired by "https://forums.minecraftforge.net/topic/11596-add-a-field-to-a-base-class/?do=findComment&comment=61923".
+ */
 public interface INoCubesBlockState {
 
 	void setTerrainSmoothable(boolean value);
