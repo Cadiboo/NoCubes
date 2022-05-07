2d04dbe43e97f8861d079ed9708fc91370d3b31b
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2022-05-07 17:37:36 +1000

    Use minecraft's GameTests TestRunner instead of our own custom implementation

---
+++ src/test/java/io/github/cadiboo/nocubes/integrationtesting/GameTestsAdapter.java
@@ -0,0 +1,38 @@
+package io.github.cadiboo.nocubes.integrationtesting;
+
+import io.github.cadiboo.nocubes.NoCubes;
+import net.minecraft.gametest.framework.GameTestGenerator;
+import net.minecraft.gametest.framework.TestFunction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraftforge.gametest.GameTestHolder;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@GameTestHolder(NoCubes.MOD_ID)
+final public class GameTestsAdapter {
+
+	// Refers to './run/gameTestServer/gameteststructures/empty.snbt'
+	public static final String EMPTY_STRUCTURE = new ResourceLocation(NoCubes.MOD_ID, "empty").toString();
+
+	@GameTestGenerator
+	public static Collection<TestFunction> createTests() {
+		return Arrays.stream(NoCubesTests.createTests())
+			.map(test -> new TestFunction(
+				NoCubes.MOD_ID + " integration", // batch
+				test.name(),
+				EMPTY_STRUCTURE, // structure
+				Rotation.NONE,
+				20, // maxTicks
+				20L, // setupTicks
+				true, // required
+				helper -> {
+					test.action().run();
+					helper.succeed();
+				}
+			))
+			.collect(Collectors.toList());
+	}
+}
