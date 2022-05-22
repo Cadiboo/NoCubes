a05153097f4fbf39c370cb6fcced41cc0e9713dd
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2022-05-22 13:22:00 +1000

    CI fixes

--- src/main/java/io/github/cadiboo/nocubes/hooks/INoCubesBlockState.java
+++ src/main/java/io/github/cadiboo/nocubes/hooks/INoCubesBlockState.java
@@ -2,7 +2,7 @@ package io.github.cadiboo.nocubes.hooks;
 
 /**
  * Implemented (by Mixin) on BlockStateBase.
- * Inspired by "https://forums.minecraftforge.net/topic/11596-add-a-field-to-a-base-class/?do=findComment&comment=61923".
+ * Inspired by <a href="https://forums.minecraftforge.net/topic/11596-add-a-field-to-a-base-class/?do=findComment&comment=61923">this post</a>.
  */
 public interface INoCubesBlockState {
 
