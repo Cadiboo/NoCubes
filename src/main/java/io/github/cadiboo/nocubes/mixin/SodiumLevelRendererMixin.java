27834c64cbe74ec51fffdb96a53fe3794ccad313
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2024-01-20 15:30:50 +1100

    Update SodiumLevelRendererMixin.java

--- src/main/java/io/github/cadiboo/nocubes/mixin/SodiumLevelRendererMixin.java
+++ src/main/java/io/github/cadiboo/nocubes/mixin/SodiumLevelRendererMixin.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.Pseudo;
  * There is more to this mixin, see the documentation in {@link io.github.cadiboo.nocubes.hooks.MixinAsm}
  * and {@link io.github.cadiboo.nocubes.hooks.MixinAsm#transformSodiumLevelRenderer}.
  */
-@Pseudo // Sodium may not be installed
-@Mixin(targets = "net.minecraft.client.renderer.LevelRenderer")
+@Mixin(net.minecraft.client.renderer.LevelRenderer.class)
 public class SodiumLevelRendererMixin {
 }
