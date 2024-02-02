7310911ee622490d8012622d5bdd8bbaedcf04a5
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2024-02-02 19:30:36 +1100

    Update DoNotRunThisFromTheConsoleDialog.java

--- src/main/java/io/github/cadiboo/nocubes/DoNotRunThisFromTheConsoleDialog.java
+++ src/main/java/io/github/cadiboo/nocubes/DoNotRunThisFromTheConsoleDialog.java
@@ -1,10 +1,7 @@
 package io.github.cadiboo.nocubes;
 
-import javax.swing.JOptionPane;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import java.awt.Desktop;
-import java.awt.GraphicsEnvironment;
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 
@@ -16,10 +13,11 @@ public final class DoNotRunThisFromTheConsoleDialog {
 	public static void main(String... args) {
 		final var modName = "NoCubes";
 		final var installLinkBtnLabel = "Help";
+		final var modLoaderName = "Forge";
 		final var installationUrl = "https://www.wikihow.com/Install-Minecraft-Forge";
-		final var mainMsg = "You have tried to launch " + modName + " (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Forge Loader for Minecraft, and place this file in your mods directory instead.";
-		final var guiMsg = mainMsg + "\nIf this is your first time installing mods for Forge Loader, click \"" + installLinkBtnLabel + "\" for a guide on how to do this.";
-		final var cmdMsg = mainMsg + "\nIf this is your first time installing mods for Forge Loader, open \"" + installationUrl + "\" for a guide on how to do this.";
+		final var mainMsg = "You have tried to launch " + modName + " (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install " + modLoaderName + " for Minecraft, and place this file in your mods directory instead.";
+		final var guiMsg = mainMsg + "\nIf this is your first time installing mods for " + modLoaderName + ", click \"" + installLinkBtnLabel + "\" for a guide on how to do this.";
+		final var cmdMsg = mainMsg + "\nIf this is your first time installing mods for " + modLoaderName + ", open \"" + installationUrl + "\" for a guide on how to do this.";
 		if (GraphicsEnvironment.isHeadless()) {
 			System.err.println(cmdMsg);
 		} else {
