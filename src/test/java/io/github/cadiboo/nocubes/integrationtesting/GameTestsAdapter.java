47021caeab1e03b682f1f3f5447f0cd1806f6d25
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2024-04-26 21:32:01 +1000

    Fabric prep: Integration test changes

--- src/test/java/io/github/cadiboo/nocubes/integrationtesting/GameTestsAdapter.java
+++ src/test/java/io/github/cadiboo/nocubes/integrationtesting/GameTestsAdapter.java
@@ -1,23 +1,19 @@
 package io.github.cadiboo.nocubes.integrationtesting;
 
 import io.github.cadiboo.nocubes.NoCubes;
-import net.minecraft.gametest.framework.GameTestGenerator;
 import net.minecraft.gametest.framework.TestFunction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Rotation;
-import net.minecraftforge.gametest.GameTestHolder;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-@GameTestHolder(NoCubes.MOD_ID)
 final public class GameTestsAdapter {
 
 	// Refers to './run/gameTestServer/gameteststructures/empty.snbt'
 	public static final String EMPTY_STRUCTURE = new ResourceLocation(NoCubes.MOD_ID, "empty").toString();
 
-	@GameTestGenerator
 	public static Collection<TestFunction> createTests() {
 		return Arrays.stream(NoCubesTests.createTests())
 			.map(test -> new TestFunction(
